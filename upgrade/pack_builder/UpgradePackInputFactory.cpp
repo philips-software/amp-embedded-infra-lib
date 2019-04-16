0848a33937f2a032502a313170a33f12fbc13f41
Ron Jaegers <ron.jaegers@philips.com>
2019-04-16 15:28:17 +0200

    upgrade/pack_builder/UpgradePackInputFactory: replace std::find by std::any_of

--- upgrade/pack_builder/UpgradePackInputFactory.cpp
+++ upgrade/pack_builder/UpgradePackInputFactory.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "upgrade/pack_builder/InputBinary.hpp"
 #include "upgrade/pack_builder/InputHex.hpp"
 #include "upgrade/pack_builder/UpgradePackInputFactory.hpp"
@@ -46,7 +47,7 @@ namespace application
 
     std::unique_ptr<Input> UpgradePackInputFactory::CreateInput(const std::string& targetName, const std::string& fileName)
     {
-        if (std::find(supportedHexTargets.begin(), supportedHexTargets.end(), targetName) != supportedHexTargets.end())
+        if (std::any_of(supportedHexTargets.begin(), supportedHexTargets.end(), [targetName](auto& string) { return string == targetName; }))
             return std::make_unique<InputHex>(targetName, fileName, fileSystem, imageSecurity);
 
         for (auto targetAndAddress : supportedBinaryTargets)
