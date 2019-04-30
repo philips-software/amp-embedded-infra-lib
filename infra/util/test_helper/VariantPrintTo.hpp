a7220746ae782e36831ef7f15b2964fd2adc8c92
Richard Peters <richard@rpeters.org>
2019-04-30 20:50:27 +0200

    infra/util/test_helper: Add VariantPrintTo

---
+++ infra/util/test_helper/VariantPrintTo.hpp
@@ -0,0 +1,37 @@
+#ifndef INFRA_VARIANT_PRINT_TO_HPP
+#define INFRA_VARIANT_PRINT_TO_HPP
+
+#include "gtest/gtest-printers.h"
+#include "infra/util/Variant.hpp"
+
+namespace infra {
+    using testing::internal::PrintTo;
+
+    struct PrintToVisitor
+    {
+        using ResultType = void;
+
+        PrintToVisitor(std::ostream* os)
+            : os(os)
+        {}
+
+        template<class T>
+        void operator()(const T& value)
+        {
+            PrintTo(value, os);
+        }
+
+    private:
+        std::ostream* os;
+    };
+
+    template<class... T>
+    void PrintTo(const Variant<T...>& variant, std::ostream* os)
+    {
+        *os << "Variant[";
+        ApplyVisitor(PrintToVisitor(os), variant);
+        *os << ']';
+    }
+}
+
+#endif
