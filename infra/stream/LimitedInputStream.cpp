0e67908ee74b90b0ebc92282b47fa817b502c581
Richard Peters <richard@rpeters.org>
2019-06-29 12:30:21 +0200

    infra/stream/LimitedInputStream: Add LimitedStreamReaderWithRewinding

--- infra/stream/LimitedInputStream.cpp
+++ infra/stream/LimitedInputStream.cpp
@@ -51,4 +51,67 @@ namespace infra
     {
         return std::min<uint32_t>(length, input.Available());
     }
+
+    LimitedStreamReaderWithRewinding::LimitedStreamReaderWithRewinding(StreamReaderWithRewinding& input, uint32_t length)
+        : input(input)
+        , length(length)
+    {}
+
+    LimitedStreamReaderWithRewinding::LimitedStreamReaderWithRewinding(const LimitedStreamReaderWithRewinding& other)
+        : input(other.input)
+        , length(other.length)
+    {}
+
+    void LimitedStreamReaderWithRewinding::Extract(ByteRange range, StreamErrorPolicy& errorPolicy)
+    {
+        errorPolicy.ReportResult(length >= range.size());
+        range.shrink_from_back_to(length);
+        length -= range.size();
+        input.Extract(range, errorPolicy);
+    }
+
+    uint8_t LimitedStreamReaderWithRewinding::Peek(StreamErrorPolicy& errorPolicy)
+    {
+        errorPolicy.ReportResult(length != 0);
+
+        if (length != 0)
+            return input.Peek(errorPolicy);
+        else
+            return 0;
+    }
+
+    ConstByteRange LimitedStreamReaderWithRewinding::ExtractContiguousRange(std::size_t max)
+    {
+        ConstByteRange result = input.ExtractContiguousRange(std::min<std::size_t>(length, max));
+        length -= result.size();
+        return result;
+    }
+
+    ConstByteRange infra::LimitedStreamReaderWithRewinding::PeekContiguousRange(std::size_t start)
+    {
+        return input.PeekContiguousRange(start);
+    }
+
+    bool LimitedStreamReaderWithRewinding::Empty() const
+    {
+        return length == 0 || input.Empty();
+    }
+
+    std::size_t LimitedStreamReaderWithRewinding::Available() const
+    {
+        return std::min<uint32_t>(length, input.Available());
+    }
+
+    std::size_t LimitedStreamReaderWithRewinding::ConstructSaveMarker() const
+    {
+        return input.ConstructSaveMarker();
+    }
+
+    void LimitedStreamReaderWithRewinding::Rewind(std::size_t marker)
+    {
+        auto now = input.ConstructSaveMarker();
+        input.Rewind(marker);
+
+        length += now - marker;
+    }
 }
