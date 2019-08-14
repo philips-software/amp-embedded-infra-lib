661d900c781afe8d94dec315d178b7f55b2d47e4
Ron Jaegers <ron.jaegers@philips.com>
2019-08-14 15:02:00 +0200

    infra/timer/Timer: fix time conversion with strftime.

--- infra/timer/Timer.hpp
+++ infra/timer/Timer.hpp
@@ -115,9 +115,9 @@ namespace std
     {
         std::time_t now_c = std::chrono::system_clock::to_time_t(p);
         std::tm tm = *gmtime(&now_c);
-#if defined(__GNUC__) && __GNUC__ <= 4
+#if !defined(__clang__) && defined(__GNUC__) && __GNUC__ <= 4
         char formattedTime[24];
-        if (strftime(formattedTime, sizeof(formattedTime), "%F %T.] ", now_c) > 0)
+        if (strftime(formattedTime, sizeof(formattedTime), "%F %T.] ", &tm) > 0)
             *os << formattedTime;
 #else
         uint64_t fractional = std::chrono::duration_cast<std::chrono::nanoseconds>(p - std::chrono::time_point_cast<std::chrono::seconds>(p)).count();
