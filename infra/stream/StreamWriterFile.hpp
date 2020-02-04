b15ec792c209ba17660a1925e7fb402c5846f027
Richard Peters <richard.peters@philips.com>
2020-02-04 12:46:33 +0100

    infra/stream: Add StreamWriterFile

---
+++ infra/stream/StreamWriterFile.hpp
@@ -0,0 +1,24 @@
+#ifndef INFRA_STREAM_WRITER_FILE_HPP
+#define INFRA_STREAM_WRITER_FILE_HPP
+
+#include "infra/stream/OutputStream.hpp"
+#include <cstdio>
+
+namespace infra
+{
+    class StreamWriterFile
+        : public StreamWriter
+    {
+    public:
+        StreamWriterFile(const char* path);
+        ~StreamWriterFile();
+
+        virtual void Insert(ConstByteRange range, StreamErrorPolicy& errorPolicy) override;
+        virtual std::size_t Available() const override;
+
+    private:
+        FILE* stream;
+    };
+}
+
+#endif
