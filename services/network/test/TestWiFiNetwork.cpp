5e621b995d0c48d07ced87908eddaa7bb0ce3065
Richard Peters <richard.peters@philips.com>
2020-03-26 08:09:12 +0100

    Fix incorrect assertions

--- services/network/test/TestWiFiNetwork.cpp
+++ services/network/test/TestWiFiNetwork.cpp
@@ -6,22 +6,22 @@ TEST(WiFiNetworkTest, WiFiSecurity_prints_correct_security_type)
     services::WiFiSecurity security;
     security.securityMode = services::WiFiSecurity::SecurityMode::unknown;
 
-    ASSERT_EQ("unknown", security.ToString());
-    ASSERT_EQ("open", services::WiFiSecurity{}.ToString());
-    ASSERT_EQ("wep", services::WiFiSecurity::WepSecurity("").ToString());
-    ASSERT_EQ("wpa", services::WiFiSecurity::WpaSecurity("").ToString());
-    ASSERT_EQ("wpa-2", services::WiFiSecurity::Wpa2Security("").ToString());
-    ASSERT_EQ("wpa-3", services::WiFiSecurity::Wpa3Security("").ToString());
+    EXPECT_EQ("unknown", security.ToString());
+    EXPECT_EQ("open", services::WiFiSecurity{}.ToString());
+    EXPECT_EQ("wep", services::WiFiSecurity::WepSecurity("").ToString());
+    EXPECT_EQ("wpa", services::WiFiSecurity::WpaSecurity("").ToString());
+    EXPECT_EQ("wpa-2", services::WiFiSecurity::Wpa2Security("").ToString());
+    EXPECT_EQ("wpa-3", services::WiFiSecurity::Wpa3Security("").ToString());
 }
 
 TEST(WiFiNetworkTest, WiFiSecurity_is_comparable)
 {
-    ASSERT_TRUE(services::WiFiSecurity::WpaSecurity("12345678") == services::WiFiSecurity::WpaSecurity("12345678"));
-    ASSERT_FALSE(services::WiFiSecurity::WpaSecurity("12345678") != services::WiFiSecurity::WpaSecurity("12345678"));
+    EXPECT_TRUE(services::WiFiSecurity::WpaSecurity("12345678") == services::WiFiSecurity::WpaSecurity("12345678"));
+    EXPECT_FALSE(services::WiFiSecurity::WpaSecurity("12345678") != services::WiFiSecurity::WpaSecurity("12345678"));
 
-    ASSERT_FALSE(services::WiFiSecurity::WpaSecurity("") == services::WiFiSecurity::WpaSecurity("12345678"));
-    ASSERT_TRUE(services::WiFiSecurity::WpaSecurity("") != services::WiFiSecurity::WpaSecurity("12345678"));
+    EXPECT_FALSE(services::WiFiSecurity::WpaSecurity("") == services::WiFiSecurity::WpaSecurity("12345678"));
+    EXPECT_TRUE(services::WiFiSecurity::WpaSecurity("") != services::WiFiSecurity::WpaSecurity("12345678"));
 
-    ASSERT_FALSE(services::WiFiSecurity::Wpa2Security("12345678") == services::WiFiSecurity::WpaSecurity("12345678"));
-    ASSERT_TRUE(services::WiFiSecurity::Wpa2Security("12345678") != services::WiFiSecurity::WpaSecurity("12345678"));
+    EXPECT_FALSE(services::WiFiSecurity::Wpa2Security("12345678") == services::WiFiSecurity::WpaSecurity("12345678"));
+    EXPECT_TRUE(services::WiFiSecurity::Wpa2Security("12345678") != services::WiFiSecurity::WpaSecurity("12345678"));
 }
