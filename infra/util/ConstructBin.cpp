937fa2b9c16b9d429183a60c14c945a42b5437f0
Richard Peters <richard.peters@philips.com>
2020-08-04 13:50:55 +0200

    infra/util: Add ConstructBin

---
+++ infra/util/ConstructBin.cpp
@@ -0,0 +1,32 @@
+#include "infra/util/ConstructBin.hpp"
+
+namespace infra
+{
+    ConstructBin& ConstructBin::operator()(const std::vector<uint8_t>& v)
+    {
+        contents.insert(contents.end(), v.begin(), v.end());
+        return *this;
+    }
+
+    ConstructBin& ConstructBin::operator()(const std::string& v)
+    {
+        contents.insert(contents.end(), reinterpret_cast<const uint8_t*>(v.data()), reinterpret_cast<const uint8_t*>(v.data()) + v.size());
+        return *this;
+    }
+
+    ConstructBin& ConstructBin::operator()(uint8_t v)
+    {
+        contents.push_back(v);
+        return *this;
+    }
+
+    std::vector<uint8_t> ConstructBin::Vector() const
+    {
+        return contents;
+    }
+
+    ConstByteRange ConstructBin::Range() const
+    {
+        return MakeRange(contents);
+    }
+}
---
+++ infra/util/ConstructBin.hpp
@@ -0,0 +1,38 @@
+#ifndef INFRA_CONSTRUCT_BIN_HPP
+#define INFRA_CONSTRUCT_BIN_HPP
+
+#include "infra/util/ByteRange.hpp"
+#include <string>
+#include <vector>
+
+namespace infra
+{
+    class ConstructBin
+    {
+    public:
+        ConstructBin& operator()(const std::vector<uint8_t>& v);
+        ConstructBin& operator()(const std::string& v);
+        ConstructBin& operator()(uint8_t v);
+
+        template<class T>
+            ConstructBin& Value(T v);
+
+        std::vector<uint8_t> Vector() const;
+        ConstByteRange Range() const;
+
+    private:
+        std::vector<uint8_t> contents;
+    };
+
+    ////    Implementation    ////
+
+    template<class T>
+    ConstructBin& ConstructBin::Value(T v)
+    {
+        auto range = infra::MakeByteRange(v);
+        contents.insert(contents.end(), range.begin(), range.end());
+        return *this;
+    }
+}
+
+#endif
