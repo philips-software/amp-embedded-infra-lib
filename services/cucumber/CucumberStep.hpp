1bc45ce89aed2e83a73518201b6c3ab4b88761cf
Ron Jaegers <ron.jaegers@philips.com>
2021-06-25 16:04:28 +0200

    Reverted visitor parser since it was causing corruption.

--- services/cucumber/CucumberStep.hpp
+++ services/cucumber/CucumberStep.hpp
@@ -4,7 +4,6 @@
 #include "infra/syntax/Json.hpp"
 #include "infra/util/BoundedString.hpp"
 #include "infra/util/IntrusiveList.hpp"
-#include "infra/syntax/JsonStreamingParser.hpp"
 #include "services/cucumber/CucumberContext.hpp"
 
 namespace services
@@ -22,8 +21,8 @@ namespace services
 
         infra::BoundedConstString StepName() const;
 
-        infra::Optional<infra::JsonString> GetTableArgument(infra::BoundedConstString fieldName);
-        bool ContainsTableArgument(infra::BoundedConstString fieldName);
+        infra::Optional<infra::JsonString> GetTableArgument(const infra::BoundedString& fieldName);
+        bool ContainsTableArgument(const infra::BoundedString& fieldName);
         infra::Optional<infra::JsonString> GetStringArgument(uint8_t argumentNumber);
         bool HasStringArguments() const;
         bool ContainsStringArgument(uint8_t index);
@@ -36,43 +35,9 @@ namespace services
         infra::JsonArray* invokeArguments = nullptr;
 
     private:
+        void IterateThroughStringArguments(infra::JsonArrayIterator& iterator);
+
         infra::BoundedConstString stepName;
-    };
-
-    class StringArgumentVisitor
-        : public infra::JsonArrayVisitor
-    {
-    public:
-        StringArgumentVisitor(size_t argNumber);
-
-        virtual void VisitString(infra::BoundedConstString value) override;
-        virtual JsonArrayVisitor* VisitArray(infra::JsonSubArrayParser& parser) override;
-
-        infra::Optional<infra::JsonString>& MatchedArgument();
-    private:
-        bool tableArgument = false;
-        size_t argNumber = 0;
-        size_t argCount = 0;
-        infra::BoundedString::WithStorage<48> matchBuffer;
-        infra::Optional<infra::JsonString> matchedArgument = infra::none;
-    };
-
-    class TableArgumentVisitor
-        : public infra::JsonArrayVisitor
-    {
-    public:
-        TableArgumentVisitor(infra::BoundedConstString field);
-
-        virtual void VisitString(infra::BoundedConstString value) override;
-        virtual JsonArrayVisitor* VisitArray(infra::JsonSubArrayParser& parser) override;
-
-        infra::Optional<infra::JsonString>& MatchedArgument();
-
-    private:
-        bool tableArgument = false;
-        bool match = false;
-        infra::BoundedConstString field;
-        infra::Optional<infra::JsonString> matchedArgument = infra::none;
     };
 }
 
