5e494af2ca1f06bcdb9c2d629dbdcb4278851a7a
Richard Peters <richard.peters@philips.com>
2021-11-08 14:15:26 +0100

    infra/stream/CountingInputStream: Add CountingStreamReaderWithRewinding

--- infra/stream/CountingInputStream.hpp
+++ infra/stream/CountingInputStream.hpp
@@ -33,6 +33,36 @@ namespace infra
         uint32_t totalRead = 0;
     };
 
+    class CountingStreamReaderWithRewinding
+        : public infra::StreamReaderWithRewinding
+        , private infra::StreamErrorPolicy
+    {
+    public:
+        CountingStreamReaderWithRewinding(infra::StreamReaderWithRewinding& reader);
+
+        uint32_t TotalRead() const;
+
+        // Implementation of StreamReaderWithRewinding
+        virtual void Extract(infra::ByteRange range, infra::StreamErrorPolicy& errorPolicy) override;
+        virtual uint8_t Peek(infra::StreamErrorPolicy& errorPolicy) override;
+        virtual infra::ConstByteRange ExtractContiguousRange(std::size_t max) override;
+        virtual infra::ConstByteRange PeekContiguousRange(std::size_t start) override;
+        virtual bool Empty() const override;
+        virtual std::size_t Available() const override;
+        virtual std::size_t ConstructSaveMarker() const override;
+        virtual void Rewind(std::size_t marker) override;
+
+        // Implementation of StreamErrorPolicy
+        virtual bool Failed() const override;
+        virtual void ReportResult(bool ok) override;
+
+    private:
+        infra::StreamReaderWithRewinding& reader;
+        infra::StreamErrorPolicy* otherErrorPolicy = nullptr;
+        bool ok = true;
+        uint32_t totalRead = 0;
+    };
+
     class CountingTextInputStream
         : public TextInputStream::WithReader<CountingStreamReader>
     {
