21a57e780dbb8b1541dc17dce17119b74e693a14
Richard Peters <richard.peters@philips.com>
2021-12-22 11:21:43 +0100

    infra/util: Add MultiFunction and AutoResetMultiFunction

---
+++ infra/util/AutoResetMultiFunction.hpp
@@ -0,0 +1,128 @@
+#ifndef INFRA_AUTO_RESET_MULTI_FUNCTION_HPP
+#define INFRA_AUTO_RESET_MULTI_FUNCTION_HPP
+
+#include "infra/util/MultiFunction.hpp"
+#include "infra/util/PostAssign.hpp"
+
+namespace infra
+{
+    template<class... F>
+    class AutoResetMultiFunctionHelper
+    {
+    public:
+        template<class FOther>
+        using And = AutoResetMultiFunctionHelper<F..., infra::Function<FOther>>;
+
+        AutoResetMultiFunctionHelper() = default;
+        explicit AutoResetMultiFunctionHelper(std::nullptr_t);
+        AutoResetMultiFunctionHelper(const AutoResetMultiFunctionHelper& other) = delete;
+        AutoResetMultiFunctionHelper(AutoResetMultiFunctionHelper&& other);
+        template<class F1>
+            AutoResetMultiFunctionHelper(F1&& f);
+
+        AutoResetMultiFunctionHelper& operator=(const AutoResetMultiFunctionHelper& other) = delete;
+        AutoResetMultiFunctionHelper& operator=(AutoResetMultiFunctionHelper&& other);
+        AutoResetMultiFunctionHelper& operator=(std::nullptr_t);
+
+        template<class... Args>
+            typename MultiFunctionHelper<F...>::template ResultOf<Args...>::Type operator()(Args&&... args);
+
+        explicit operator bool() const;
+
+        MultiFunctionHelper<F...> Clone() const;
+
+    private:
+        MultiFunctionHelper<F...> multiFunction;
+    };
+
+    template<class... F>
+        bool operator==(const AutoResetMultiFunctionHelper<F...>& f, std::nullptr_t);
+    template<class... F>
+        bool operator==(std::nullptr_t, const AutoResetMultiFunctionHelper<F...>& f);
+    template<class... F>
+        bool operator!=(const AutoResetMultiFunctionHelper<F...>& f, std::nullptr_t);
+    template<class... F>
+        bool operator!=(std::nullptr_t, const AutoResetMultiFunctionHelper<F...>& f);
+
+    template<class F>
+    using AutoResetMultiFunction = AutoResetMultiFunctionHelper<infra::Function<F>>;
+
+    ////    Implementation    ////
+
+    template<class... F>
+    AutoResetMultiFunctionHelper<F...>::AutoResetMultiFunctionHelper(std::nullptr_t)
+    {}
+
+    template<class... F>
+    AutoResetMultiFunctionHelper<F...>::AutoResetMultiFunctionHelper(AutoResetMultiFunctionHelper&& other)
+        : multiFunction(other.multiFunction)
+    {
+        other.multiFunction = nullptr;
+    }
+
+    template<class... F>
+    template<class F1>
+    AutoResetMultiFunctionHelper<F...>::AutoResetMultiFunctionHelper(F1&& f)
+        : multiFunction(std::forward<F1>(f))
+    {}
+
+    template<class... F>
+    AutoResetMultiFunctionHelper<F...>& AutoResetMultiFunctionHelper<F...>::operator=(std::nullptr_t)
+    {
+        multiFunction = nullptr;
+        return *this;
+    }
+
+    template<class... F>
+    AutoResetMultiFunctionHelper<F...>& AutoResetMultiFunctionHelper<F...>::operator=(AutoResetMultiFunctionHelper&& other)
+    {
+        multiFunction = other.multiFunction;
+        other.multiFunction = nullptr;
+        return *this;
+    }
+
+    template<class... F>
+    template<class... Args>
+    typename MultiFunctionHelper<F...>::template ResultOf<Args...>::Type AutoResetMultiFunctionHelper<F...>::operator()(Args&&... args)
+    {
+        return infra::PostAssign(multiFunction, nullptr)(std::forward<Args>(args)...);
+    }
+
+    template<class... F>
+    AutoResetMultiFunctionHelper<F...>::operator bool() const
+    {
+        return multiFunction != nullptr;
+    }
+
+    template<class... F>
+    infra::MultiFunctionHelper<F...> AutoResetMultiFunctionHelper<F...>::Clone() const
+    {
+        return multiFunction;
+    }
+
+    template<class... F>
+    bool operator==(const AutoResetMultiFunctionHelper<F...>& f, std::nullptr_t)
+    {
+        return !f;
+    }
+
+    template<class... F>
+    bool operator==(std::nullptr_t, const AutoResetMultiFunctionHelper<F...>& f)
+    {
+        return !f;
+    }
+
+    template<class... F>
+    bool operator!=(const AutoResetMultiFunctionHelper<F...>& f, std::nullptr_t)
+    {
+        return !(f == nullptr);
+    }
+
+    template<class... F>
+    bool operator!=(std::nullptr_t, const AutoResetMultiFunctionHelper<F...>& f)
+    {
+        return !(f == nullptr);
+    }
+}
+
+#endif
---
+++ infra/util/MultiFunction.hpp
@@ -0,0 +1,123 @@
+#ifndef INFRA_MULTI_FUNCTION_HPP
+#define INFRA_MULTI_FUNCTION_HPP
+
+#include "infra/util/Function.hpp"
+#include "infra/util/Variant.hpp"
+
+namespace infra
+{
+    template<class Fn, class L>
+    struct IsInvocableList;
+
+    template<class Fn, class... Args>
+    struct IsInvocableList<Fn, List<Args...>>
+        : std::is_invocable<Fn, Args...>
+    {};
+
+    template<class... F>
+    class MultiFunctionHelper
+    {
+    public:
+        template<class FOther>
+        using And = MultiFunctionHelper<F..., infra::Function<FOther>>;
+
+        MultiFunctionHelper() = default;
+        MultiFunctionHelper(std::nullptr_t) {}
+
+        template<class... Args>
+        struct IndexOf
+        {
+            template<std::size_t V, class Enable, class... FOthers>
+            struct Helper;
+
+            template<std::size_t V, class F0, class... FOthers>
+            struct Helper<V, std::true_type, F0, FOthers...>
+            {
+                static const std::size_t Value = V;
+            };
+
+            template<std::size_t V, class F0, class F1, class... FOthers>
+            struct Helper<V, std::false_type, F0, F1, FOthers...>
+            {
+                static const std::size_t Value = Helper<V + 1, typename std::is_invocable<F1, Args...>::type, F1, FOthers...>::Value;
+            };
+
+            static const std::size_t Value = Helper<0, typename std::is_invocable<typename infra::Front<F...>::Type, Args...>::type, F...>::Value;
+        };
+
+        template<class F2>
+        struct IndexOfFunctionObject
+        {
+            template<std::size_t V, class Enable, class... FOthers>
+            struct Helper;
+
+            template<std::size_t V, class F0, class... FOthers>
+            struct Helper<V, std::true_type, F0, FOthers...>
+            {
+                static const std::size_t Value = V;
+            };
+
+            template<std::size_t V, class F0, class F1, class... FOthers>
+            struct Helper<V, std::false_type, F0, F1, FOthers...>
+            {
+                static const std::size_t Value = Helper<V + 1, typename IsInvocableList<F2, typename F1::Arguments>::type, F1, FOthers...>::Value;
+            };
+
+            static const std::size_t Value = Helper<0, typename IsInvocableList<F2, typename infra::Front<F...>::Type::Arguments>::type, F...>::Value;
+        };
+
+        template<class F2>
+        MultiFunctionHelper(F2 f)
+        {
+            functions.Emplace<typename infra::TypeAtIndex<IndexOfFunctionObject<F2>::Value, F...>::Type>(f);
+        }
+
+        template<class... Args>
+        struct ResultOf
+        {
+            using Type = typename infra::TypeAtIndex<IndexOf<Args...>::Value, F...>::Type::ResultType;
+        };
+
+        template<class... Args>
+        typename ResultOf<Args...>::Type operator()(Args&&... args)
+        {
+            return functions.GetAtIndex<IndexOf<Args...>::Value + 1>()(std::forward<Args>(args)...);
+        }
+
+        template<class... Args>
+        bool Invocable([[maybe_unused]] Args&&...args)
+        {
+            return functions.Which() == IndexOf<Args...>::Value + 1;
+        }
+
+        explicit operator bool() const { return functions.Which() != 0; }
+
+        bool operator==(const nullptr_t) const
+        {
+            return functions.Which() == 0;
+        }
+
+        friend bool operator==(const nullptr_t, const MultiFunctionHelper<F...>& f)
+        {
+            return f.functions.Which() == 0;
+        }
+
+        bool operator!=(const nullptr_t) const
+        {
+            return functions.Which() != 0;
+        }
+
+        friend bool operator!=(const nullptr_t, const MultiFunctionHelper<F...>& f)
+        {
+            return f.functions.Which() != 0;
+        }
+
+    private:
+        infra::Variant<infra::None, F...> functions;
+    };
+
+    template<class F>
+    using MultiFunction = MultiFunctionHelper<infra::Function<F>>;
+}
+
+#endif
