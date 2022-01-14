b7e5d0b86eccf7c6276e89a57182c4d3ba3a7b06
Richard Peters <richard.peters@philips.com>
2022-01-14 12:07:03 +0100

    infra/util/CMakeLists: Add emil_target_range_source

--- infra/util/transform_file_to_range.cmake
+++ infra/util/transform_file_to_range.cmake
@@ -7,5 +7,5 @@ string(LENGTH "${contents}" contentsLength)
 
 string(REGEX REPLACE "(..)" "'\\\\x\\1', " contents "${contents}")
 
-configure_file("${script_dir}/transform_file_to_string.cpp.conf" generated/${namespace}/${output}.cpp)
-configure_file("${script_dir}/transform_file_to_string.hpp.conf" generated/${namespace}/${output}.hpp)
+configure_file("${script_dir}/transform_file_to_range.cpp.conf" generated/${namespace}/${output}.cpp)
+configure_file("${script_dir}/transform_file_to_range.hpp.conf" generated/${namespace}/${output}.hpp)
