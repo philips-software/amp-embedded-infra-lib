90bd9060ffff3e004ff049d07de10d418e099993
Ron Jaegers <ron.jaegers@philips.com>
2022-01-18 15:31:30 +0100

    Minor changes to toolchain file
    - Renamed CCOLA to EMIL
    - Removed name with .exe; CMake already fixes this for us

--- cmake/toolchain-arm-gcc.cmake
+++ cmake/toolchain-arm-gcc.cmake
@@ -13,55 +13,44 @@ endif()
 
 find_program(CMAKE_C_COMPILER
     NAMES arm-none-eabi-gcc
-          arm-none-eabi-gcc.exe
     HINTS ${hints_paths})
 
 find_program(CMAKE_CXX_COMPILER
     NAMES arm-none-eabi-g++
-          arm-none-eabi-g++.exe
     HINTS ${hints_paths})
 
 find_program(CMAKE_ASM_COMPILER
     NAMES arm-none-eabi-as
-          arm-none-eabi-as.exe
     HINTS ${hints_paths})
 
 find_program(CMAKE_AR
     NAMES arm-none-eabi-ar
-          arm-none-eabi-ar.exe
     HINTS ${hints_paths})
 
 find_program(CMAKE_RANLIB
     NAMES arm-none-eabi-ranlib
-          arm-none-eabi-ranlib.exe
     HINTS ${hints_paths})
 
 find_program(CMAKE_NM
     NAMES arm-none-eabi-nm
-          arm-none-eabi-nm.exe
     HINTS ${hints_paths})
 
-find_program(CCOLA_LD_TOOL
+find_program(EMIL_LD_TOOL
     NAMES arm-none-eabi-ld
-          arm-none-eabi-ld.exe
     HINTS ${hints_paths})
 
-find_program(CCOLA_APP_SIZE_TOOL
+find_program(EMIL_APP_SIZE_TOOL
     NAMES arm-none-eabi-size
-          arm-none-eabi-size.exe
     HINTS ${hints_paths})
 
-find_program(CCOLA_OBJ_COPY_TOOL
+find_program(EMIL_OBJ_COPY_TOOL
     NAMES arm-none-eabi-objcopy
-          arm-none-eabi-objcopy.exe
     HINTS ${hints_paths})
 
-find_program(CCOLA_OBJ_DUMP_TOOL
+find_program(EMIL_OBJ_DUMP_TOOL
     NAMES arm-none-eabi-objdump
-          arm-none-eabi-objdump.exe
     HINTS ${hints_paths})
 
-find_program(CCOLA_DEBUG_TOOL
+find_program(EMIL_DEBUG_TOOL
     NAMES arm-none-eabi-gdb
-          arm-none-eabi-gdb.exe
     HINTS ${hints_paths})
