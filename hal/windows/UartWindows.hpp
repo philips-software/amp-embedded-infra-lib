7e284271902d744cda3462f7a607bd811a57f2e5
Daan Timmer <daan.timmer@gmail.com>
2022-03-09 21:47:42 +0100

    Fixed an issue with a class with a non-static data member being used as a default parameter argument

--- hal/windows/UartWindows.hpp
+++ hal/windows/UartWindows.hpp
@@ -49,7 +49,7 @@ namespace hal
                 space = SPACEPARITY
             };
 
-            UartWindowsConfig() = default;
+            UartWindowsConfig(){}
 
             UartWindowsConfig(uint32_t newbaudRate, RtsFlowControl newFlowControlRts)
                 : baudRate(newbaudRate)
