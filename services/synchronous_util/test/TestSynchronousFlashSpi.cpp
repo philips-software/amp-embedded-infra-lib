8f32eec64719024d17d74e428c8fcc3c6f87cbe1
Roy Creemers <roy.creemers@philips.com>
2022-05-04 17:28:36 +0200

    Add 4-byte addressing support for synchronous SPI flash

--- services/synchronous_util/test/TestSynchronousFlashSpi.cpp
+++ services/synchronous_util/test/TestSynchronousFlashSpi.cpp
@@ -5,11 +5,11 @@
 class SynchronousFlashSpiTest
     : public testing::Test
 {
-public:
-    SynchronousFlashSpiTest()
-        : flashSpi(spiMock)
-    {
-    }
+protected:
+    SynchronousFlashSpiTest(bool extendedAddressing)
+        : config{ 512, extendedAddressing }
+        , flashSpi(spiMock, config)
+    {}
 
     void ExpectWriteEnable()
     {
@@ -22,13 +22,22 @@ public:
         EXPECT_CALL(spiMock, ReceiveDataMock(hal::SynchronousSpi::Action::stop)).WillOnce(testing::Return(std::vector<uint8_t>{ 0 }));
     }
 
+    services::SynchronousFlashSpi::Config config;
     testing::StrictMock<hal::SynchronousSpiMock> spiMock;
     services::SynchronousFlashSpi flashSpi;
 
     testing::InSequence s;
 };
 
-TEST_F(SynchronousFlashSpiTest, WriteBuffer)
+class SynchronousFlashSpiTest3ByteAddressing : public SynchronousFlashSpiTest
+{
+public:
+    SynchronousFlashSpiTest3ByteAddressing()
+        : SynchronousFlashSpiTest(false)
+    {}
+};
+
+TEST_F(SynchronousFlashSpiTest3ByteAddressing, WriteBuffer)
 {
     ExpectWriteEnable();
     EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x02, 0x12, 0x34, 0x56 }, hal::SynchronousSpi::Action::continueSession));
@@ -39,7 +48,7 @@ TEST_F(SynchronousFlashSpiTest, WriteBuffer)
     flashSpi.WriteBuffer(data, 0x123456);
 }
 
-TEST_F(SynchronousFlashSpiTest, ReadBuffer)
+TEST_F(SynchronousFlashSpiTest3ByteAddressing, ReadBuffer)
 {
     EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x03, 0x12, 0x34, 0x56 }, hal::SynchronousSpi::Action::continueSession));
     EXPECT_CALL(spiMock, ReceiveDataMock(hal::SynchronousSpi::Action::stop)).WillOnce(testing::Return(std::vector<uint8_t>{ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 }));
@@ -50,7 +59,7 @@ TEST_F(SynchronousFlashSpiTest, ReadBuffer)
     ASSERT_THAT(buffer, testing::ElementsAre(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07));
 }
 
-TEST_F(SynchronousFlashSpiTest, EraseSectors)
+TEST_F(SynchronousFlashSpiTest3ByteAddressing, EraseSectors)
 {
     ExpectWriteEnable();
     EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x20, 0x00, 0x00, 0x00 }, hal::SynchronousSpi::Action::stop));
@@ -63,7 +72,7 @@ TEST_F(SynchronousFlashSpiTest, EraseSectors)
     flashSpi.EraseSectors(0, 2);
 }
 
-TEST_F(SynchronousFlashSpiTest, ReadFlashId)
+TEST_F(SynchronousFlashSpiTest3ByteAddressing, ReadFlashId)
 {
     EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x9f }, hal::SynchronousSpi::Action::continueSession));
     EXPECT_CALL(spiMock, ReceiveDataMock(hal::SynchronousSpi::Action::stop)).WillOnce(testing::Return(std::vector<uint8_t>{ 0x01, 0x02, 0x03 }));
@@ -73,3 +82,58 @@ TEST_F(SynchronousFlashSpiTest, ReadFlashId)
 
     ASSERT_THAT(flashId, testing::ElementsAre(0x01, 0x02, 0x03));
 }
+
+class SynchronousFlashSpiTest4ByteAddressing : public SynchronousFlashSpiTest
+{
+public:
+    SynchronousFlashSpiTest4ByteAddressing()
+        : SynchronousFlashSpiTest(true)
+    {}
+};
+
+TEST_F(SynchronousFlashSpiTest4ByteAddressing, WriteBuffer)
+{
+    ExpectWriteEnable();
+    EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x12, 0x12, 0x34, 0x56, 0x78 }, hal::SynchronousSpi::Action::continueSession));
+    EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0, 1, 2, 3, 4, 5, 6, 7 }, hal::SynchronousSpi::Action::stop));
+    ExpectHoldWhileWriteInProgress();
+
+    std::array<uint8_t, 8> data = { 0, 1, 2, 3, 4, 5, 6, 7 };
+    flashSpi.WriteBuffer(data, 0x12345678);
+}
+
+TEST_F(SynchronousFlashSpiTest4ByteAddressing, ReadBuffer)
+{
+    EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x13, 0x12, 0x34, 0x56, 0x78 }, hal::SynchronousSpi::Action::continueSession));
+    EXPECT_CALL(spiMock, ReceiveDataMock(hal::SynchronousSpi::Action::stop)).WillOnce(testing::Return(std::vector<uint8_t>{ 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07 }));
+
+    std::array<uint8_t, 8> buffer;
+    flashSpi.ReadBuffer(buffer, 0x12345678);
+
+    ASSERT_THAT(buffer, testing::ElementsAre(0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07));
+}
+
+TEST_F(SynchronousFlashSpiTest4ByteAddressing, EraseSectors)
+{
+    ExpectWriteEnable();
+    EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x21, 0x00, 0x00, 0x00, 0x00 }, hal::SynchronousSpi::Action::stop));
+    ExpectHoldWhileWriteInProgress();
+
+    ExpectWriteEnable();
+    EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x21, 0x00, 0x00, 0x10, 0x00 }, hal::SynchronousSpi::Action::stop));
+    ExpectHoldWhileWriteInProgress();
+
+    flashSpi.EraseSectors(0, 2);
+}
+
+TEST_F(SynchronousFlashSpiTest4ByteAddressing, ReadFlashId)
+{
+    EXPECT_CALL(spiMock, SendDataMock(std::vector<uint8_t>{ 0x9f }, hal::SynchronousSpi::Action::continueSession));
+    EXPECT_CALL(spiMock, ReceiveDataMock(hal::SynchronousSpi::Action::stop)).WillOnce(testing::Return(std::vector<uint8_t>{ 0x01, 0x02, 0x03 }));
+
+    std::array<uint8_t, 3> flashId;
+    flashSpi.ReadFlashId(flashId);
+
+    ASSERT_THAT(flashId, testing::ElementsAre(0x01, 0x02, 0x03));
+}
+
