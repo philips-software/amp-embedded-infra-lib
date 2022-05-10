5d38dab63c7de18696bc330586ad28ab55c0aa44
Oguzcan Oguz <oguz.oguzcan@philips.com>
2022-05-10 16:36:49 +0200

    Format ADType names

--- services/ble/CoreSpec.hpp
+++ services/ble/CoreSpec.hpp
@@ -11,36 +11,36 @@ namespace services
         //Core Specification Supplement, Part A, Section 1.3
         enum class ADType : uint8_t
         {
-            FLAGS                                = 0x01U,
-            INCMPLT_LIST_16_BIT_SERV_UUID        = 0x02U,
-            CMPLT_LIST_16_BIT_SERV_UUID          = 0x03U,
-            INCMPLT_LIST_32_BIT_SERV_UUID        = 0x04U,
-            CMPLT_LIST_32_BIT_SERV_UUID          = 0x05U,
-            INCMPLT_LIST_128_BIT_SERV_UUID       = 0x06U,
-            CMPLT_LIST_128_BIT_SERV_UUID         = 0x07U,
-            SHORTENED_LOCAL_NAME                 = 0x08U,
-            COMPLETE_LOCAL_NAME                  = 0x09U,
-            TX_POWER_LEVEL                       = 0x0AU,
-            CLASS_OF_DEVICE                      = 0x0DU,
-            SEC_MGR_TK_VALUE                     = 0x10U,
-            SEC_MGR_OOB_FLAGS                    = 0x11U,
-            SLAVE_CONN_INTERVAL                  = 0x12U,
-            SERV_SOLICIT_16_BIT_UUID_LIST        = 0x14U,
-            SERV_SOLICIT_128_BIT_UUID_LIST       = 0x15U,
-            SERVICE_DATA                         = 0x16U,
-            APPEARANCE                           = 0x19U,
-            ADVERTISING_INTERVAL                 = 0x1AU,
-            LE_ROLE                              = 0x1CU,
-            SERV_SOLICIT_32_BIT_UUID_LIST        = 0x1FU,
-            URI                                  = 0x24U,
-            MANUFACTURER_SPECIFIC_DATA           = 0xFFU
+            Flags = 0x01u,
+            IncmpltList16BitServUuid = 0x02u,
+            CmpltList16BitServUuid = 0x03u,
+            IncmpltList32BitServUuid = 0x04u,
+            CmpltList32BitServUuid = 0x05u,
+            IncmpltList128BitServUuid = 0x06u,
+            CmpltList128BitServUuid = 0x07u,
+            ShortenedLocalName = 0x08u,
+            CompleteLocalName = 0x09u,
+            TxPowerLevel = 0x0au,
+            ClassOfDevice = 0x0du,
+            SecMgrTkValue = 0x10u,
+            SecMgrOobFlags = 0x11u,
+            SlaveConnInterval = 0x12u,
+            ServSolicit16BitUuidList = 0x14u,
+            ServSolicit128BitUuidList = 0x15u,
+            ServiceData = 0x16u,
+            Appearance = 0x19u,
+            AdvertisingInterval = 0x1au,
+            LeRole = 0x1cu,
+            ServSolicit_32BitUuidList = 0x1fu,
+            Uri = 0x24u,
+            ManufacturerSpecificData = 0xffu
         };
 
         //Core Specification Supplement, Part A, Section 1.3
         typedef ADType SRDType;
 
-        static constexpr uint8_t maxAdvertisementSize = 31;
-        static constexpr uint8_t maxScanResponseSize = 31;
+        static constexpr uint8_t MaxAdvertisementSize = 31;
+        static constexpr uint8_t MaxScanResponseSize = 31;
     };
 }
 
