3ce7ebeb52ab882f072127ee67eaf73ec4b573d2
Richard Peters <richard.peters@philips.com>
2022-08-10 10:17:40 +0200

    cmake/emil_version_generator.cmake: Generate Version.h with uppercase V

--- cmake/emil_version_generator.cmake
+++ cmake/emil_version_generator.cmake
@@ -13,17 +13,17 @@ function(add_version_header_target target_name)
 
     add_custom_target(${target_name}
         COMMAND ${CMAKE_COMMAND} -D VERSION_INPUT_FILE=${CMAKE_CURRENT_FUNCTION_LIST_DIR}/version.h.in
-                                 -D VERSION_OUTPUT_FILE=${CMAKE_BINARY_DIR}/${target_name}/generated/version.h
+                                 -D VERSION_OUTPUT_FILE=${CMAKE_BINARY_DIR}/${target_name}/generated/Version.h
                                  -D CMAKE_PROJECT_NAME=${CMAKE_PROJECT_NAME}
                                  -D PROJECT_SOURCE_DIR=${PROJECT_SOURCE_DIR}
                                  -D VERSION_MAJOR=${${CMAKE_PROJECT_NAME}_VERSION_MAJOR}
                                  -D VERSION_MINOR=${${CMAKE_PROJECT_NAME}_VERSION_MINOR}
                                  -D VERSION_PATCH=${${CMAKE_PROJECT_NAME}_VERSION_PATCH}
                                  -P ${CMAKE_CURRENT_FUNCTION_LIST_DIR}/version_file_generator.cmake
-        BYPRODUCTS ${CMAKE_BINARY_DIR}/${target_name}/generated/version.h
+        BYPRODUCTS ${CMAKE_BINARY_DIR}/${target_name}/generated/Version.h
         COMMENT "Generating version header for ${CMAKE_PROJECT_NAME}"
         VERBATIM
-        SOURCES ${CMAKE_BINARY_DIR}/${target_name}/generated/version.h)
+        SOURCES ${CMAKE_BINARY_DIR}/${target_name}/generated/Version.h)
 
     set_target_properties(${target_name} PROPERTIES VERSION_DIR ${CMAKE_BINARY_DIR}/${target_name})
 endfunction()
