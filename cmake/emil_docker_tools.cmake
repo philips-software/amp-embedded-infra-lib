180f6941b24a1f4f93acf8bbd55e61dfdc5ed507
Ron Jaegers <ron.jaegers@philips.com>
2022-09-28 14:45:55 +0200

    cmake/emil_docker_tools.cmake: add support for host Docker tools
    When running outside of a devcontainer, but Docker is installed add the option to run additional tools directly on the host.

--- cmake/emil_docker_tools.cmake
+++ cmake/emil_docker_tools.cmake
@@ -1,10 +1,27 @@
+# This module will include Docker tools as custom targets in a build-tree.
+#
+# Three scenario's are supported:
+# - Running inside a devcontainer using a volume-mount (e.g. clone in container volume)
+# - Running inside a devcontainer using a bind-mount (e.g. (re-)open in container)
+# - Running native with Docker installed on the host
+#
+# The first two scenarios require a Docker cli to be installed inside the container,
+# the Docker socket exposed inside that container and the workspace location exported
+# as environment variable (LOCAL_WORKSPACE_FOLDER).
+# See: https://github.com/microsoft/vscode-dev-containers/tree/main/containers/docker-from-docker
+#
+# The third scenario directly interacts with the Docker instance on the host machine.
+
 find_program(DOCKER docker)
 
 if (DOCKER)
+    cmake_host_system_information(RESULT hostname QUERY HOSTNAME)
+
     execute_process(
-        COMMAND ${DOCKER} inspect -f "{{ range .Mounts }}{{ if eq .Destination \"/workspaces\" }}{{ .Name }}{{ end }}{{ end }}" $ENV{HOSTNAME}
+        COMMAND ${DOCKER} inspect -f "{{ range .Mounts }}{{ if eq .Destination \"/workspaces\" }}{{ .Name }}{{ end }}{{ end }}" ${hostname}
         OUTPUT_VARIABLE docker_volume
         OUTPUT_STRIP_TRAILING_WHITESPACE
+        ERROR_QUIET
     )
 
     if (NOT docker_volume STREQUAL "")
@@ -18,7 +35,9 @@ if (DOCKER)
         cmake_path(GET docker_mountpoint PARENT_PATH docker_mountpoint_parent)
         cmake_path(GET docker_mountpoint STEM docker_mountpoint_stem)
     else()
-        message(WARNING "Could not determine paths and mount-points for Docker tools")
+        string(REPLACE "\\" "/" docker_mountpoint ${CMAKE_SOURCE_DIR})
+        cmake_path(GET docker_mountpoint PARENT_PATH docker_mountpoint_parent)
+        cmake_path(GET docker_mountpoint STEM docker_mountpoint_stem)
     endif()
 
     add_custom_target(lint
