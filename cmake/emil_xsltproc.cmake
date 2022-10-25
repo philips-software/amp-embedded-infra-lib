5c50c13062ff098bb730daffad2ccd4e42deaa57
Ron Jaegers <ron.jaegers@philips.com>
2022-10-25 12:55:47 +0000

    external/xsltproc: download with FetchContent

--- cmake/emil_xsltproc.cmake
+++ cmake/emil_xsltproc.cmake
@@ -1,5 +1,16 @@
-function(generate_xslt target output)
+function(emil_fetch_xsltproc)
+    if (CMAKE_HOST_WIN32 AND NOT EMIL_XSLTPROC_PATH)
+        FetchContent_Declare(xsltproc
+            URL https://www.zlatkovic.com/pub/libxml/libxslt-1.1.26.win32.zip
+        )
 
+        FetchContent_MakeAvailable(xsltproc)
+
+        set(EMIL_XSLTPROC_PATH "${xsltproc_SOURCE_DIR}/bin/" CACHE INTERNAL "")
+    endif()
+endfunction()
+
+function(generate_xslt target output)
     set(inputs ${ARGV})
     list(REMOVE_AT inputs 0 1)
 
@@ -26,9 +37,11 @@ function(generate_xslt target output)
     cmake_path(ABSOLUTE_PATH output BASE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR} NORMALIZE OUTPUT_VARIABLE absolute_output)
     cmake_path(GET absolute_output PARENT_PATH absolute_directory)
 
+    emil_fetch_xsltproc()
+
     find_program(xsltproc_program
         NAMES xsltproc
-        HINTS ${CMAKE_CURRENT_FUNCTION_LIST_DIR}
+        HINTS ${EMIL_XSLTPROC_PATH}
     )
 
     add_custom_command(
@@ -39,8 +52,4 @@ function(generate_xslt target output)
     )
 
     target_sources(${target} PRIVATE ${absolute_inputs})
-
-    unset(absolute_inputs)
-    unset(xslt_params)
-
 endfunction()
