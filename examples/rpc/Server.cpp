0e0d709b7354c4050ea9bbfba95bf206f8f30450
Ron <45816308+rjaegers@users.noreply.github.com>
2022-12-20 07:38:22 +0100

    feat: add rpc example (#141)

---
+++ examples/rpc/Server.cpp
@@ -0,0 +1,65 @@
+#include "generated/echo/Console.pb.hpp"
+#include "hal/generic/TimerServiceGeneric.hpp"
+#include "protobuf/echo/Echo.hpp"
+#include "services/network_instantiations/NetworkAdapter.hpp"
+#include <iostream>
+
+class Console
+    : public examples::Console
+{
+public:
+    Console(services::Echo& echo)
+        : examples::Console(echo)
+    {}
+
+    void Write(infra::BoundedConstString message) override
+    {
+        std::cout << infra::AsStdString(message) << std::endl;
+        MethodDone();
+    }
+};
+
+class EchoConnection
+    : public services::EchoOnConnection
+{
+public:
+    EchoConnection()
+        : console(*this)
+    {}
+
+private:
+    Console console;
+};
+
+class RpcServer
+    : public services::ServerConnectionObserverFactory
+{
+public:
+    RpcServer(services::ConnectionFactory& factory)
+        : listener(factory.Listen(1234, *this))
+    {}
+
+    void ConnectionAccepted(infra::AutoResetFunction<void(infra::SharedPtr<services::ConnectionObserver> connectionObserver)>&& createdObserver, services::IPAddress address) override
+    {
+        if (connection)
+            connection->::services::EchoOnConnection::Subject().AbortAndDestroy();
+
+        if (connection.Allocatable())
+            createdObserver(connection.Emplace());
+    }
+
+private:
+    infra::SharedPtr<void> listener;
+    infra::SharedOptional<EchoConnection> connection;
+};
+
+int main(int argc, const char* argv[], const char* env[])
+{
+    static hal::TimerServiceGeneric timerService;
+    static main_::NetworkAdapter network;
+    static RpcServer rpc(network.ConnectionFactory());
+
+    network.Run();
+
+    return 0;
+}
