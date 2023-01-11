4ccb5135ee6407f4a6b19cf1c88749725e22f396
Richard Peters <richard.peters@philips.com>
2023-01-11 15:55:26 +0100

    cmake/emil_folderize.cmake: Prefix functions with "emil_"

--- cmake/emil_folderize.cmake
+++ cmake/emil_folderize.cmake
@@ -1,8 +1,8 @@
-# Three helper functions are introduced, the main helper function is folderize_all_targets()
+# Three helper functions are introduced, the main helper function is emil_folderize_all_targets()
 # Using this function, all targets which have a name of the format package.target have their
 # FOLDER property set to package, so that in supported IDEs those targets are combined into folders.
 
-function(get_subdirectories foundDirectories topDir)
+function(emil_get_subdirectories foundDirectories topDir)
     set(found)
     get_property(subDirectories DIRECTORY ${topDir} PROPERTY SUBDIRECTORIES)
     while(subDirectories)
@@ -16,8 +16,8 @@ function(get_subdirectories foundDirectories topDir)
     set(${foundDirectories} ${found} PARENT_SCOPE)
 endfunction()
 
-function(get_project_targets foundTargets)
-    get_subdirectories(directories ${PROJECT_SOURCE_DIR})
+function(emil_get_project_targets foundTargets)
+    emil_get_subdirectories(directories ${PROJECT_SOURCE_DIR})
 
     foreach(dir ${directories})
         get_property(subDirTargets DIRECTORY ${dir} PROPERTY BUILDSYSTEM_TARGETS)
@@ -26,8 +26,8 @@ function(get_project_targets foundTargets)
     set(${foundTargets} ${targets} PARENT_SCOPE)
 endfunction()
 
-function(folderize_all_targets)
-    get_project_targets(targets)
+function(emil_folderize_all_targets)
+    emil_get_project_targets(targets)
 
     foreach(target ${targets})
         string(REGEX MATCH [^\.]+ component ${target})
