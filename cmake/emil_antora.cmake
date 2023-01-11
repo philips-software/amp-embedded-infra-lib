aeadc8e9c8e72aeef2139819104c0bf16c20c936
Richard Peters <richard.peters@philips.com>
2023-01-11 15:50:41 +0100

    cmake/emil_antora.cmake: Prefix function with "emil_"

--- cmake/emil_antora.cmake
+++ cmake/emil_antora.cmake
@@ -1,4 +1,4 @@
-function(generate_antora_docs_target playbook)
+function(emil_generate_antora_docs_target playbook)
     add_custom_target(generate_antora_docs
         COMMAND antora --cache-dir=.cache --stacktrace --fetch --generator=antora-site-generator-lunr ${playbook}
         WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
