11a92e0133746a25a505873813ea950778eaaba2
Timmer, Daan <daan.timmer@philips.com>
2023-01-19 13:53:13 +0100

    Added infra::ToMacAddrewss

--- hal/interfaces/MacAddress.cpp
+++ hal/interfaces/MacAddress.cpp
@@ -8,7 +8,16 @@ namespace infra
 
     AsMacAddressHelper AsMacAddress(hal::MacAddress macAddress)
     {
-        return AsMacAddressHelper(macAddress);
+        return AsMacAddressHelper{ macAddress };
+    }
+
+    AsMacAddressWriterHelper::AsMacAddressWriterHelper(hal::MacAddress& macAddress)
+        : macAddress(macAddress)
+    {}
+
+    AsMacAddressWriterHelper ToMacAddress(hal::MacAddress& macAddress)
+    {
+        return AsMacAddressWriterHelper{ macAddress };
     }
 
     TextOutputStream& operator<<(TextOutputStream& stream, const AsMacAddressHelper& asMacAddressHelper)
@@ -28,5 +37,18 @@ namespace infra
     TextOutputStream& operator<<(TextOutputStream&& stream, const AsMacAddressHelper& asMacAddressHelper)
     {
         return stream << asMacAddressHelper;
+    }
+
+    TextInputStream& operator>>(TextInputStream& stream, AsMacAddressWriterHelper& asMacAddressHelper)
+    {
+        auto& mac = asMacAddressHelper.macAddress;
+        const auto w02 = Width(2, '0');
+        stream >> hex >> w02 >> mac[0] >> resetWidth >> ":" >> w02 >> mac[1] >> resetWidth >> ":" >> w02 >> mac[2] >> resetWidth >> ":" >> w02 >> mac[3] >> resetWidth >> ":" >> w02 >> mac[4] >> resetWidth >> ":" >> w02 >> mac[5];
+        return stream;
+    }
+
+    TextInputStream& operator>>(TextInputStream&& stream, AsMacAddressWriterHelper& asMacAddressHelper)
+    {
+        return stream >> asMacAddressHelper;
     }
 }
--- hal/interfaces/MacAddress.hpp
+++ hal/interfaces/MacAddress.hpp
@@ -1,6 +1,7 @@
 #ifndef HAL_MAC_ADDRESS_HPP
 #define HAL_MAC_ADDRESS_HPP
 
+#include "infra/stream/InputStream.hpp"
 #include "infra/stream/OutputStream.hpp"
 #include <array>
 
@@ -23,6 +24,19 @@ namespace infra
         hal::MacAddress macAddress;
     };
 
+    class AsMacAddressWriterHelper
+    {
+    public:
+        explicit AsMacAddressWriterHelper(hal::MacAddress& macAddress);
+
+        friend TextInputStream& operator>>(TextInputStream& stream, AsMacAddressWriterHelper& asMacAddressHelper);
+        friend TextInputStream& operator>>(TextInputStream&& stream, AsMacAddressWriterHelper& asMacAddressHelper);
+
+    private:
+        hal::MacAddress& macAddress;
+    };
+
     AsMacAddressHelper AsMacAddress(hal::MacAddress macAddress);
+    AsMacAddressWriterHelper ToMacAddress(hal::MacAddress& macAddress);
 }
 #endif
