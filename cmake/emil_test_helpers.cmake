668688038bcd53c19fbabeb048b93faa8ec7ad96
Daan Timmer <8293597+daantimmer@users.noreply.github.com>
2023-02-07 14:21:38 +0100

    ci: Replaced mull-ir-frontend-12 with mull-ir-frontend (#197)
    ci: Replaced mull-ir-frontend-12 with mull-ir-frontend when for clang-15 and higher

--- cmake/emil_test_helpers.cmake
+++ cmake/emil_test_helpers.cmake
@@ -34,10 +34,19 @@ function(emil_enable_testing)
 
     if (EMIL_ENABLE_MUTATION_TESTING)
         if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-            add_compile_options(
-                -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping
-                -fexperimental-new-pass-manager -fpass-plugin=/usr/lib/mull-ir-frontend-12
-            )
+            execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE CLANG_VERSION)
+
+            if(CLANG_VERSION VERSION_GREATER 15.0 OR CLANG_VERSION VERSION_EQUAL 15.0)
+                add_compile_options(
+                    -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping
+                    -fexperimental-new-pass-manager -fpass-plugin=/usr/lib/mull-ir-frontend
+                )
+            else()
+                add_compile_options(
+                    -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping
+                    -fexperimental-new-pass-manager -fpass-plugin=/usr/lib/mull-ir-frontend-12
+                )
+            endif()
 
             add_link_options(-fprofile-instr-generate)
         else()
