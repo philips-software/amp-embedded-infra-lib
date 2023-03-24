3e53660b48f3440bd64711bf1c1c47df20edfe60
Gabriel Santos <gabriel.santos_1@philips.com>
2023-03-24 16:48:03 +0100

    [ECR] UUID128 changed back to big endian

--- services/ble/Gatt.hpp
+++ services/ble/Gatt.hpp
@@ -14,7 +14,7 @@ namespace services
     struct AttAttribute
     {
         using Uuid16 = uint16_t;
-        using Uuid128 = std::array<uint8_t, 16>;
+        using Uuid128 = infra::BigEndian<std::array<uint8_t, 16>>;
         using Uuid = infra::Variant<Uuid16, Uuid128>;
 
         using Handle = uint16_t;
