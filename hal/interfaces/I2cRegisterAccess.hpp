e49e2810fe3ecfe6a4670afb002f6b4b2b7051aa
Roy Creemers <roy.creemers@philips.com>
2023-03-29 10:39:12 +0200

    hal/interfaces/I2cRegisterAccess: Add support for 16-bit register address

--- hal/interfaces/I2cRegisterAccess.hpp
+++ hal/interfaces/I2cRegisterAccess.hpp
@@ -6,19 +6,20 @@
 
 namespace hal
 {
+    template<class T>
     class I2cMasterRegisterAccess
     {
     public:
         I2cMasterRegisterAccess(I2cMaster& i2cMaster, I2cAddress address);
 
-        void ReadRegister(uint8_t dataRegister, infra::ByteRange data, const infra::Function<void()>& onDone);
-        void WriteRegister(uint8_t dataRegister, infra::ConstByteRange data, const infra::Function<void()>& onDone);
+        void ReadRegister(T dataRegister, infra::ByteRange data, const infra::Function<void()>& onDone);
+        void WriteRegister(T dataRegister, infra::ConstByteRange data, const infra::Function<void()>& onDone);
 
     private:
         I2cMaster& i2cMaster;
         I2cAddress address;
 
-        uint8_t dataRegister = 0;
+        T dataRegister = 0;
         infra::ByteRange readData;
         infra::ConstByteRange writeData;
         infra::Function<void()> onDone;
