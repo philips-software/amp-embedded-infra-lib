e39959dacbaa35991fe684f179259e0158114797
Stef Helling <stef.helling@philips.com>
2023-05-11 15:00:26 +0000

    Made GetAdvertisementData and GetScanResponseData functions in Gap const

--- services/ble/Gap.hpp
+++ services/ble/Gap.hpp
@@ -198,9 +198,9 @@ namespace services
         virtual GapAddress GetAddress() const = 0;
         virtual GapAddress GetIdentityAddress() const = 0;
         virtual void SetAdvertisementData(infra::ConstByteRange data) = 0;
-        virtual infra::ConstByteRange GetAdvertisementData() = 0;
+        virtual infra::ConstByteRange GetAdvertisementData() const = 0;
         virtual void SetScanResponseData(infra::ConstByteRange data) = 0;
-        virtual infra::ConstByteRange GetScanResponseData() = 0;
+        virtual infra::ConstByteRange GetScanResponseData() const = 0;
         virtual void Advertise(GapAdvertisementType type, AdvertisementIntervalMultiplier multiplier) = 0;
         virtual void Standby() = 0;
     };
@@ -220,9 +220,9 @@ namespace services
         virtual GapAddress GetIdentityAddress() const override;
 
         virtual void SetAdvertisementData(infra::ConstByteRange data) override;
-        virtual infra::ConstByteRange GetAdvertisementData() override;
+        virtual infra::ConstByteRange GetAdvertisementData() const override;
         virtual void SetScanResponseData(infra::ConstByteRange data) override;
-        virtual infra::ConstByteRange GetScanResponseData() override;
+        virtual infra::ConstByteRange GetScanResponseData() const override;
         virtual void Advertise(GapAdvertisementType type, AdvertisementIntervalMultiplier multiplier) override;
         virtual void Standby() override;
     };
