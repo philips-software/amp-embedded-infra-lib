d4fd27e68cf6f272274bb40e681025457f321957
Rob Ekelmans <rob.ekelmans@philips.com>
2023-06-23 09:30:34 +0000

    Add streaming to and from LittleEndianMacAddress

--- hal/interfaces/MacAddress.cpp
+++ hal/interfaces/MacAddress.cpp
@@ -6,20 +6,6 @@ namespace infra
         : macAddress(macAddress)
     {}
 
-    AsMacAddressHelper AsMacAddress(hal::MacAddress macAddress)
-    {
-        return AsMacAddressHelper{ macAddress };
-    }
-
-    AsMacAddressWriterHelper::AsMacAddressWriterHelper(hal::MacAddress& macAddress)
-        : macAddress(macAddress)
-    {}
-
-    AsMacAddressWriterHelper ToMacAddress(hal::MacAddress& macAddress)
-    {
-        return AsMacAddressWriterHelper{ macAddress };
-    }
-
     TextOutputStream& operator<<(TextOutputStream& stream, const AsMacAddressHelper& asMacAddressHelper)
     {
         auto& mac = asMacAddressHelper.macAddress;
@@ -39,7 +25,11 @@ namespace infra
         return stream << asMacAddressHelper;
     }
 
-    TextInputStream& operator>>(TextInputStream& stream, AsMacAddressWriterHelper asMacAddressHelper)
+    ToMacAddressHelper::ToMacAddressHelper(hal::MacAddress& macAddress)
+        : macAddress(macAddress)
+    {}
+
+    TextInputStream& operator>>(TextInputStream& stream, ToMacAddressHelper asMacAddressHelper)
     {
         auto& mac = asMacAddressHelper.macAddress;
         const auto w02 = Width(2, '0');
@@ -47,8 +37,46 @@ namespace infra
         return stream;
     }
 
-    TextInputStream& operator>>(TextInputStream&& stream, AsMacAddressWriterHelper asMacAddressHelper)
+    TextInputStream& operator>>(TextInputStream&& stream, ToMacAddressHelper asMacAddressHelper)
     {
         return stream >> asMacAddressHelper;
+    }
+
+    ToLittleEndianMacAddressHelper::ToLittleEndianMacAddressHelper(hal::MacAddress& macAddress)
+        : macAddress(macAddress)
+    {}
+
+    TextInputStream& operator>>(TextInputStream& stream, ToLittleEndianMacAddressHelper asMacAddressHelper)
+    {
+        auto& mac = asMacAddressHelper.macAddress;
+        const auto w02 = Width(2, '0');
+        stream >> hex >> w02 >> mac[5] >> resetWidth >> ":" >> w02 >> mac[4] >> resetWidth >> ":" >> w02 >> mac[3] >> resetWidth >> ":" >> w02 >> mac[2] >> resetWidth >> ":" >> w02 >> mac[1] >> resetWidth >> ":" >> w02 >> mac[0];
+        return stream;
+    }
+
+    TextInputStream& operator>>(TextInputStream&& stream, ToLittleEndianMacAddressHelper asMacAddressHelper)
+    {
+        return stream >> asMacAddressHelper;
+    }
+
+    AsMacAddressHelper AsMacAddress(hal::MacAddress macAddress)
+    {
+        return AsMacAddressHelper{ macAddress };
+    }
+
+    ToMacAddressHelper ToMacAddress(hal::MacAddress& macAddress)
+    {
+        return ToMacAddressHelper{ macAddress };
+    }
+
+    AsMacAddressHelper AsLittleEndianMacAddress(hal::MacAddress macAddress)
+    {
+        std::reverse(std::begin(macAddress), std::end(macAddress));
+        return AsMacAddressHelper{ macAddress };
+    }
+
+    ToLittleEndianMacAddressHelper ToLittleEndianMacAddress(hal::MacAddress& macAddress)
+    {
+        return ToLittleEndianMacAddressHelper{ macAddress };
     }
 }
