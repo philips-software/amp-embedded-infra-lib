3c7a6309d38612d2e51371d9fe4a6a1c89043327
Ron <45816308+rjaegers@users.noreply.github.com>
2023-06-29 09:39:27 +0200

    Check on stack-use-after-scope

--- infra/syntax/fuzz/FuzzJson.cpp
+++ infra/syntax/fuzz/FuzzJson.cpp
@@ -7,7 +7,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, std::size_t size)
     FuzzedDataProvider provider(data, size);
     infra::JsonObject object(provider.ConsumeRandomLengthString());
 
-    for (const auto& o : object)
+    for (auto o : object)
     {}
 
     return 0;
