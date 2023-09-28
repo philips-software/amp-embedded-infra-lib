248eab84c24acb9194431fc4c9c75aaffbc87a39
Ron Jaegers <ron.jaegers@philips.com>
2023-09-28 09:19:37 +0000

    chore: remove workaround for missing symlinks
    When running into issues using mutation testing, please update your local devcontainer.

--- cmake/emil_test_helpers.cmake
+++ cmake/emil_test_helpers.cmake
@@ -34,19 +34,10 @@ function(emil_enable_testing)
 
     if (EMIL_ENABLE_MUTATION_TESTING)
         if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-            execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE CLANG_VERSION)
-
-            if(CLANG_VERSION VERSION_GREATER 15.0 OR CLANG_VERSION VERSION_EQUAL 15.0)
-                add_compile_options(
-                    -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping
-                    -fexperimental-new-pass-manager -fpass-plugin=/usr/lib/mull-ir-frontend
-                )
-            else()
-                add_compile_options(
-                    -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping
-                    -fexperimental-new-pass-manager -fpass-plugin=/usr/lib/mull-ir-frontend-12
-                )
-            endif()
+            add_compile_options(
+                -g -O0 -grecord-command-line -fprofile-instr-generate -fcoverage-mapping
+                -fexperimental-new-pass-manager -fpass-plugin=/usr/lib/mull-ir-frontend
+            )
 
             add_link_options(-fprofile-instr-generate)
         else()
@@ -68,13 +59,7 @@ function(emil_add_test target)
     get_target_property(exclude ${target} EXCLUDE_FROM_ALL)
     if (NOT ${exclude})
         if (EMIL_ENABLE_MUTATION_TESTING)
-            execute_process(COMMAND ${CMAKE_CXX_COMPILER} -dumpversion OUTPUT_VARIABLE CLANG_VERSION)
-
-            if(CLANG_VERSION VERSION_GREATER 15.0 OR CLANG_VERSION VERSION_EQUAL 15.0)
-                add_test(NAME ${target} COMMAND mull-runner ${EMIL_MUTATION_TESTING_RUNNER_ARGUMENTS} $<TARGET_FILE:${target}>)
-            else()
-                add_test(NAME ${target} COMMAND mull-runner-12 ${EMIL_MUTATION_TESTING_RUNNER_ARGUMENTS} $<TARGET_FILE:${target}>)
-            endif()
+            add_test(NAME ${target} COMMAND mull-runner ${EMIL_MUTATION_TESTING_RUNNER_ARGUMENTS} $<TARGET_FILE:${target}>)
         else()
             add_test(NAME ${target} COMMAND ${target})
         endif()
