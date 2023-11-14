42c64c649236e54d2d0c997213ae0c7530508847
Richard Peters <richard.peters@philips.com>
2023-11-14 14:10:38 +0100

    protobuf/echo/test_doubles/EchoSingleLoopback: Handle multiple sent messages, and release reader upon destruction

--- protobuf/echo/Echo.hpp
+++ protobuf/echo/Echo.hpp
@@ -84,6 +84,7 @@ namespace services
 
         void SendStreamAvailable(infra::SharedPtr<infra::StreamWriter>&& writer);
         void DataReceived(infra::SharedPtr<infra::StreamReaderWithRewinding>&& reader);
+        void ReleaseReader();
 
     private:
         void TryGrantSend();
