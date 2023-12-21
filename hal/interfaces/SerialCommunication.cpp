a295022702918f2a0dde982a6659c4a04efcb2cc
Richard Peters <richard@rpeters.org>
2023-12-21 12:39:45 +0100

    hal/interfaces/SerialCommunication: Add BufferedSerialCommunication and BufferedSerialCommunicationOnUnbuffered

---
+++ hal/interfaces/SerialCommunication.cpp
@@ -0,0 +1,40 @@
+#include "hal/interfaces/SerialCommunication.hpp"
+
+namespace hal
+{
+    BufferedSerialCommunicationOnUnbuffered::BufferedSerialCommunicationOnUnbuffered(infra::AtomicByteDeque& buffer, SerialCommunication& delegate)
+        : buffer(buffer)
+        , delegate(delegate)
+    {
+        delegate.ReceiveData([this](infra::ConstByteRange data)
+            {
+                this->buffer.Push(data);
+                scheduler.Schedule([this]()
+                    {
+                        if (HasObserver())
+                            GetObserver().DataReceived();
+                    });
+            });
+    }
+
+    BufferedSerialCommunicationOnUnbuffered::~BufferedSerialCommunicationOnUnbuffered()
+    {
+        delegate.ReceiveData([](infra::ConstByteRange data) {});
+    }
+
+    void BufferedSerialCommunicationOnUnbuffered::SendData(infra::ConstByteRange data, infra::Function<void()> actionOnCompletion)
+    {
+        delegate.SendData(data, actionOnCompletion);
+    }
+
+    infra::StreamReaderWithRewinding& BufferedSerialCommunicationOnUnbuffered::Reader()
+    {
+        return reader;
+    }
+
+    void BufferedSerialCommunicationOnUnbuffered::AckReceived()
+    {
+        buffer.Pop(reader.ConstructSaveMarker());
+        reader.Rewind(0);
+    }
+}
