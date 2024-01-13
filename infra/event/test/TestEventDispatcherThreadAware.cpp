b03f2413080d7c5ad50afc86880c6e427cd73a79
Richard Peters <richard@rpeters.org>
2024-01-13 07:58:02 +0100

    Add infra/event/EventDispatcherThreadAware

---
+++ infra/event/test/TestEventDispatcherThreadAware.cpp
@@ -0,0 +1,47 @@
+#include "infra/event/EventDispatcherThreadAware.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include <thread>
+
+class EventDispatcherThreadAwareIdleCounting
+    : public infra::EventDispatcherThreadAware::WithSize<50>
+{
+protected:
+    void Idle() override
+    {
+        ++idleCount;
+        infra::EventDispatcherThreadAware::WithSize<50>::Idle();
+    }
+
+public:
+    int idleCount = 0;
+};
+
+class EventDispatcherThreadAwareTest
+    : public testing::Test
+    , public EventDispatcherThreadAwareIdleCounting
+{};
+
+TEST_F(EventDispatcherThreadAwareTest, scheduled_action_is_executed)
+{
+    bool done = false;
+
+    std::thread t([&]()
+        {
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+            infra::EventDispatcher::Instance().Schedule([&]()
+                {
+                    done = true;
+                });
+        });
+
+    ExecuteUntil([&]()
+        {
+            return done == true;
+        });
+
+    EXPECT_EQ(1, idleCount);
+
+    t.join();
+}
