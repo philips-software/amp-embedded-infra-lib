bdfe7a4f0eb1f6dde0ffd9c04d90bd79d3fe2057
EkelmansPh <58972933+EkelmansPh@users.noreply.github.com>
2024-02-21 11:36:52 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- hal/interfaces/Flash.hpp
+++ hal/interfaces/Flash.hpp
@@ -27,15 +27,15 @@ namespace hal
         virtual uint32_t SizeOfSector(T sectorIndex) const = 0; // sectorIndex in range [0, number of sectors)
         T TotalSize() const;
 
-        virtual T SectorOfAddress(T address) const = 0; // address in range [0, flash size] returns in range [0, number of sectors]
+        virtual T SectorOfAddress(T address) const = 0;     // address in range [0, flash size] returns in range [0, number of sectors]
         virtual T AddressOfSector(T sectorIndex) const = 0; // sectorIndex in range [0, number of sectors]
-        T StartOfSector(T address) const; // address in range [0, flash size]
-        T AddressOffsetInSector(T address) const; // address in range [0, flash size]
-        T StartOfNextSector(T address) const; // address in range [0, flash size)
-        T StartOfPreviousSector(T address) const; // address in range [size of first sector, flash size]
-        T StartOfNextSectorCyclical(T address) const; // address in range [0, flash size)
-        T StartOfPreviousSectorCyclical(T address) const; // address in range [0, flash size]
-        bool AtStartOfSector(T address) const; // address in range [0, flash size]
+        T StartOfSector(T address) const;                   // address in range [0, flash size]
+        T AddressOffsetInSector(T address) const;           // address in range [0, flash size]
+        T StartOfNextSector(T address) const;               // address in range [0, flash size)
+        T StartOfPreviousSector(T address) const;           // address in range [size of first sector, flash size]
+        T StartOfNextSectorCyclical(T address) const;       // address in range [0, flash size)
+        T StartOfPreviousSectorCyclical(T address) const;   // address in range [0, flash size]
+        bool AtStartOfSector(T address) const;              // address in range [0, flash size]
 
         virtual void WriteBuffer(infra::ConstByteRange buffer, T address, infra::Function<void()> onDone) = 0;
         virtual void ReadBuffer(infra::ByteRange buffer, T address, infra::Function<void()> onDone) = 0;
