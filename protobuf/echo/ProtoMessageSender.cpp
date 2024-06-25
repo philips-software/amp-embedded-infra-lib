825010e359c4419ac607f30c5e31c654d4e67827
Richard Peters <richard.peters@philips.com>
2024-06-25 13:32:02 +0200

    Remove duplication

--- protobuf/echo/ProtoMessageSender.cpp
+++ protobuf/echo/ProtoMessageSender.cpp
@@ -79,71 +79,46 @@ namespace services
         return true;
     }
 
+    namespace
+    {
+        void SerializeRange(infra::BoundedVector<std::pair<uint32_t, infra::Function<bool(infra::DataOutputStream& stream, uint32_t& index, bool& retry, const infra::StreamWriter& finalWriter), 3 * sizeof(uint8_t*)>>>& stack, infra::ProtoFormatter& formatter, infra::ConstByteRange value, uint32_t fieldNumber, bool& retry)
+        {
+            formatter.PutVarInt((fieldNumber << 3) | 2);
+            formatter.PutVarInt(value.size());
+
+            stack.emplace_back(0, [value](infra::DataOutputStream& stream, uint32_t& index, const bool&, const infra::StreamWriter&)
+                {
+                    auto range = infra::Head(infra::DiscardHead(value, index), stream.Available());
+                    stream << range;
+                    index += range.size();
+                    return index == value.size();
+                });
+
+            retry = true;
+        }
+    }
+
     bool ProtoMessageSenderBase::SerializeField(ProtoStringBase, infra::ProtoFormatter& formatter, const infra::BoundedString& value, uint32_t fieldNumber, bool& retry) const
     {
-        formatter.PutVarInt((fieldNumber << 3) | 2);
-        formatter.PutVarInt(value.size());
-
-        stack.emplace_back(0, [&value](infra::DataOutputStream& stream, uint32_t& index, const bool&, const infra::StreamWriter&)
-            {
-                auto range = infra::Head(infra::DiscardHead(infra::StringAsByteRange(value), index), stream.Available());
-                stream << range;
-                index += range.size();
-                return index == value.size();
-            });
-
-        retry = true;
+        SerializeRange(stack, formatter, infra::StringAsByteRange(value), fieldNumber, retry);
         return true;
     }
 
     bool ProtoMessageSenderBase::SerializeField(ProtoUnboundedString, infra::ProtoFormatter& formatter, const std::string& value, uint32_t fieldNumber, bool& retry) const
     {
-        formatter.PutVarInt((fieldNumber << 3) | 2);
-        formatter.PutVarInt(value.size());
-
-        stack.emplace_back(0, [&value](infra::DataOutputStream& stream, uint32_t& index, const bool&, const infra::StreamWriter&)
-            {
-                auto range = infra::Head(infra::DiscardHead(infra::StdStringAsByteRange(value), index), stream.Available());
-                stream << range;
-                index += range.size();
-                return index == value.size();
-            });
-
-        retry = true;
+        SerializeRange(stack, formatter, infra::StdStringAsByteRange(value), fieldNumber, retry);
         return true;
     }
 
     bool ProtoMessageSenderBase::SerializeField(ProtoBytesBase, infra::ProtoFormatter& formatter, const infra::BoundedVector<uint8_t>& value, uint32_t fieldNumber, bool& retry) const
     {
-        formatter.PutVarInt((fieldNumber << 3) | 2);
-        formatter.PutVarInt(value.size());
-
-        stack.emplace_back(0, [&value](infra::DataOutputStream& stream, uint32_t& index, const bool&, const infra::StreamWriter&)
-            {
-                auto range = infra::Head(infra::DiscardHead(infra::MakeRange(value), index), stream.Available());
-                stream << range;
-                index += range.size();
-                return index == value.size();
-            });
-
-        retry = true;
+        SerializeRange(stack, formatter, infra::MakeRange(value), fieldNumber, retry);
         return true;
     }
 
     bool ProtoMessageSenderBase::SerializeField(ProtoUnboundedBytes, infra::ProtoFormatter& formatter, const std::vector<uint8_t>& value, uint32_t fieldNumber, bool& retry) const
     {
-        formatter.PutVarInt((fieldNumber << 3) | 2);
-        formatter.PutVarInt(value.size());
-
-        stack.emplace_back(0, [&value](infra::DataOutputStream& stream, uint32_t& index, const bool&, const infra::StreamWriter&)
-            {
-                auto range = infra::Head(infra::DiscardHead(infra::MakeRange(value), index), stream.Available());
-                stream << range;
-                index += range.size();
-                return index == value.size();
-            });
-
-        retry = true;
+        SerializeRange(stack, formatter, infra::MakeRange(value), fieldNumber, retry);
         return true;
     }
 }
