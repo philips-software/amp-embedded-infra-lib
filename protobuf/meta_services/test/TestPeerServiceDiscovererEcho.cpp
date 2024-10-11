e2c95a7f9700da3458d6d56e68239b3b755515fe
oguzcan oguz <oguz.oguzcan@philips.com>
2024-10-11 13:40:20 +0000

    PeerServiceDiscovererTest: WIP Streamlining services changed tests

--- protobuf/meta_services/test/TestPeerServiceDiscovererEcho.cpp
+++ protobuf/meta_services/test/TestPeerServiceDiscovererEcho.cpp
@@ -91,6 +91,14 @@ public:
             });
     }
 
+    void ServicesChanged(uint32_t startServiceId, uint32_t endServiceId)
+    {
+        serviceDiscoveryResponse.RequestSend([this, startServiceId, endServiceId]
+            {
+                serviceDiscoveryResponse.ServicesChanged(startServiceId, endServiceId);
+            });
+    }
+
     void StartInitialServiceDiscovery()
     {
         EXPECT_CALL(serviceDiscovery, NotifyServiceChangesMock(true));
@@ -114,9 +122,36 @@ public:
 
     using ServicesList = infra::MemoryRange<uint32_t>;
 
+    std::tuple<std::set<uint32_t>, std::set<uint32_t>> FindUpdatedSet(ServicesList oldServiceList, ServicesList newServiceList)
+    {
+        std::set<uint32_t> oldSet(oldServiceList.begin(), oldServiceList.end());
+        std::set<uint32_t> newSet(newServiceList.begin(), newServiceList.end());
+
+        std::set<uint32_t> addedServices;
+        std::set<uint32_t> removedServices;
+
+        std::set_difference(newSet.begin(), newSet.end(), oldSet.begin(), oldSet.end(), std::inserter(addedServices, addedServices.end()));
+        std::set_difference(oldSet.begin(), oldSet.end(), newSet.begin(), newSet.end(), std::inserter(removedServices, removedServices.end()));
+
+        return std::make_tuple(addedServices, removedServices);
+    }
+
+    auto FindUpdatedRange(std::set<uint32_t> range1, std::set<uint32_t> range2)
+    {
+        if (range1.empty())
+            return std::make_tuple(*range2.begin(), *range2.rbegin());
+        else if (range2.empty())
+            return std::make_tuple(*range1.begin(), *range1.rbegin());
+        else
+            return std::make_tuple(std::min(*range1.begin(), *range2.begin()), std::max(*range1.rbegin(), *range2.rbegin()));
+    }
+
     void UpdateServices(ServicesList oldServiceList, ServicesList newServiceList)
     {
-        //
+        auto [addedServices, removedServices] = FindUpdatedSet(oldServiceList, newServiceList);
+        
+        auto updatedRange = FindUpdatedRange(addedServices, removedServices);
+
     }
 };
 
@@ -145,22 +180,20 @@ TEST_F(PeerServiceDiscovererTest, ServicesChanged_triggers_rediscovery)
 
     EXPECT_CALL(serviceDiscovery, FindFirstServiceInRangeMock(0, 0));
 
-    serviceDiscoveryResponse.RequestSend([this]
-        {
-            serviceDiscoveryResponse.ServicesChanged(0, 0);
-        });
+    ServicesChanged(0, 0);
 
     EXPECT_CALL(observer, ServicesDiscovered(infra::ByteRangeContentsEqual(infra::MakeRange(services))));
     NoServiceSupported();
 
     EXPECT_CALL(serviceDiscovery, FindFirstServiceInRangeMock(5, 5));
 
-    serviceDiscoveryResponse.RequestSend([this]
-        {
-            serviceDiscoveryResponse.ServicesChanged(5, 5);
-        });
+    ServicesChanged(5, 5);
 
     std::array<uint32_t, 1> servicesUpdated{ 10 };
     EXPECT_CALL(observer, ServicesDiscovered(infra::ByteRangeContentsEqual(infra::MakeRange(servicesUpdated))));
     NoServiceSupported();
+
+    std::vector<uint32_t> oldServices{ 1, 3, 5 };
+    std::vector<uint32_t> newServices{ 5, 6, 3, 4 };
+    UpdateServices(infra::MakeRange(oldServices), infra::MakeRange(newServices));
 }
