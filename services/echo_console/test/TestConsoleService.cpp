6ad3bb74e48d921d4a47cb75ffeb694428d524a4
oguzcan oguz <oguz.oguzcan@philips.com>
2024-12-09 08:57:36 +0000

    TestConsoleService preparation

--- services/echo_console/test/TestConsoleService.cpp
+++ services/echo_console/test/TestConsoleService.cpp
@@ -1,8 +1,15 @@
+#include "protobuf/echo/test_doubles/EchoSingleLoopback.hpp"
 #include "services/echo_console/ConsoleService.hpp"
+#include "gtest/gtest.h"
 
 class ConsoleServiceTest
     : public testing::Test
 {
 public:
+    services::MethodSerializerFactory::ForServices<service_discovery::ServiceDiscovery,
+        service_discovery::ServiceDiscoveryResponse>::AndProxies<service_discovery::ServiceDiscoveryProxy,
+        service_discovery::ServiceDiscoveryResponseProxy>
+        serializerFactory;
 
+    application::EchoSingleLoopback echo{};
 };
