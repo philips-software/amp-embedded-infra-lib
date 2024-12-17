63f47ee9a0bde1d29c806708ca8f475ceea43578
Gabriel Santos <gabriel.santos_1@philips.com>
2024-12-17 11:50:39 +0100

    add oob to protobuf

--- services/ble/test/TestGapPairing.cpp
+++ services/ble/test/TestGapPairing.cpp
@@ -5,7 +5,6 @@
 #include "services/ble/test_doubles/GapPairingMock.hpp"
 #include "services/ble/test_doubles/GapPairingObserverMock.hpp"
 #include "gmock/gmock.h"
-#include <cstdint>
 
 namespace services
 {
@@ -15,6 +14,8 @@ namespace services
             : public testing::Test
         {
         public:
+            hal::MacAddress macAddress = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55 };
+            std::array<uint8_t, 16> temporary = { 0x7a, 0x1c, 0x3e, 0x5d, 0x92, 0x4b, 0x6f, 0x2a, 0x8e, 0x13, 0xbc, 0x77, 0x5f, 0x20, 0xd4, 0x61 };
             std::array<uint8_t, 16> random = { 0x2e, 0x48, 0x9d, 0x25, 0x47, 0x58, 0x53, 0x69, 0x84, 0x29, 0xc9, 0x8b, 0xd3, 0xa6, 0x41, 0xcd };
             std::array<uint8_t, 16> confirm = { 0xf4, 0x2c, 0xcc, 0x10, 0x69, 0x86, 0x95, 0x2e, 0x00, 0x89, 0x4d, 0x50, 0x3f, 0xab, 0x93, 0x39 };
 
@@ -26,7 +27,7 @@ namespace services
 
     MATCHER_P(OutOfBandDataContentsEqual, x, negation ? "Contents not equal" : "Contents are equal")
     {
-        return x.macAddress == arg.macAddress && x.addressType == arg.addressType && infra::ContentsEqual(x.randomData, arg.randomData) && infra::ContentsEqual(x.confirmData, arg.confirmData);
+        return x.macAddress == arg.macAddress && x.addressType == arg.addressType && infra::ContentsEqual(x.temporaryKey, arg.temporaryKey) && infra::ContentsEqual(x.randomData, arg.randomData) && infra::ContentsEqual(x.confirmData, arg.confirmData);
     }
 
     TEST_F(GapPairingDecoratorTest, forward_all_events_to_observers)
@@ -34,14 +35,14 @@ namespace services
         EXPECT_CALL(gapPairingObserver, DisplayPasskey(::testing::Eq(11111), ::testing::IsTrue()));
         EXPECT_CALL(gapPairingObserver, PairingSuccessfullyCompleted());
         EXPECT_CALL(gapPairingObserver, PairingFailed(::testing::TypedEq<GapPairingObserver::PairingErrorType>(GapPairingObserver::PairingErrorType::numericComparisonFailed)));
-        EXPECT_CALL(gapPairingObserver, OutOfBandDataGenerated(OutOfBandDataContentsEqual(GapPairingObserver::OutOfBandData{ hal::MacAddress(), GapDeviceAddressType::publicAddress, random, confirm })));
+        EXPECT_CALL(gapPairingObserver, OutOfBandDataGenerated(OutOfBandDataContentsEqual(GapPairingObserver::OutOfBandData{ macAddress, GapDeviceAddressType::publicAddress, temporary, random, confirm })));
 
         gapPairing.NotifyObservers([this](GapPairingObserver& obs)
             {
                 obs.DisplayPasskey(11111, true);
                 obs.PairingSuccessfullyCompleted();
                 obs.PairingFailed(GapPairingObserver::PairingErrorType::numericComparisonFailed);
-                obs.OutOfBandDataGenerated(GapPairingObserver::OutOfBandData{ hal::MacAddress(), GapDeviceAddressType::publicAddress, random, confirm });
+                obs.OutOfBandDataGenerated(GapPairingObserver::OutOfBandData{ macAddress, GapDeviceAddressType::publicAddress, temporary, random, confirm });
             });
     }
 
