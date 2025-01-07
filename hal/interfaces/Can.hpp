a74acbbb3601d1610b66124558ee60004951d7b8
Timmer, Daan <daan.timmer@philips.com>
2025-01-07 12:57:21 +0000

    chore: separate implementation from declaration

--- hal/interfaces/Can.hpp
+++ hal/interfaces/Can.hpp
@@ -3,6 +3,7 @@
 
 #include "infra/util/BoundedVector.hpp"
 #include "infra/util/Function.hpp"
+#include <cassert>
 #include <cstdint>
 
 namespace hal
@@ -13,45 +14,17 @@ namespace hal
         class [[nodiscard]] Id
         {
         public:
-            [[nodiscard]] static constexpr Id Create11BitId(uint32_t id)
-            {
-                return Can::Id(id);
-            }
+            [[nodiscard]] static constexpr Id Create11BitId(uint32_t id);
+            [[nodiscard]] static constexpr Id Create29BitId(uint32_t id);
 
-            [[nodiscard]] static constexpr Id Create29BitId(uint32_t id)
-            {
-                return Can::Id(id | indicator29Bit);
-            }
+            [[nodiscard]] constexpr bool Is11BitId() const;
+            [[nodiscard]] constexpr bool Is29BitId() const;
 
-            [[nodiscard]] constexpr bool Is11BitId() const
-            {
-                return (id & indicator29Bit) == 0;
-            }
+            [[nodiscard]] constexpr uint32_t Get11BitId() const;
+            [[nodiscard]] constexpr uint32_t Get29BitId() const;
 
-            [[nodiscard]] constexpr bool Is29BitId() const
-            {
-                return !Is11BitId();
-            }
-
-            [[nodiscard]] constexpr uint32_t Get11BitId() const
-            {
-                return id;
-            }
-
-            [[nodiscard]] constexpr uint32_t Get29BitId() const
-            {
-                return id ^ indicator29Bit;
-            }
-
-            [[nodiscard]] constexpr bool operator==(const Id& other) const
-            {
-                return id == other.id;
-            }
-
-            [[nodiscard]] constexpr bool operator!=(const Id& other) const
-            {
-                return !(*this == other);
-            }
+            [[nodiscard]] constexpr bool operator==(const Id& other) const;
+            [[nodiscard]] constexpr bool operator!=(const Id& other) const;
 
         private:
             constexpr explicit Id(uint32_t id)
@@ -77,6 +50,50 @@ namespace hal
         virtual void SendData(Id id, const Message& data, const infra::Function<void(bool success)>& actionOnCompletion) = 0;
         virtual void ReceiveData(const infra::Function<void(Id id, const Message& data)>& receivedAction) = 0;
     };
+
+    //// Implementation ////
+
+    constexpr Can::Id Can::Id::Create11BitId(uint32_t id)
+    {
+        return Can::Id(id);
+    }
+
+    constexpr Can::Id Can::Id::Create29BitId(uint32_t id)
+    {
+        return Can::Id(id | indicator29Bit);
+    }
+
+    constexpr bool Can::Id::Is11BitId() const
+    {
+        return (id & indicator29Bit) == 0;
+    }
+
+    constexpr bool Can::Id::Is29BitId() const
+    {
+        return !Is11BitId();
+    }
+
+    constexpr uint32_t Can::Id::Get11BitId() const
+    {
+        assert(Is11BitId());
+        return id;
+    }
+
+    constexpr uint32_t Can::Id::Get29BitId() const
+    {
+        assert(Is29BitId());
+        return id ^ indicator29Bit;
+    }
+
+    constexpr bool Can::Id::operator==(const Id& other) const
+    {
+        return id == other.id;
+    }
+
+    constexpr bool Can::Id::operator!=(const Id& other) const
+    {
+        return !(*this == other);
+    }
 }
 
 #endif
