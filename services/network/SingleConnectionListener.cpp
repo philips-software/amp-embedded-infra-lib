fdee637fa4234a08d945ab4e7b4447320909065f
Richard Peters <richard@rpeters.org>
2025-01-18 20:51:45 +0100

    feat: add NoneAllocated to SharedObjectAllocator

--- services/network/SingleConnectionListener.cpp
+++ services/network/SingleConnectionListener.cpp
@@ -25,10 +25,10 @@ namespace services
         this->createdObserver = std::move(createdObserver);
         this->address = address;
 
-        newConnectionStrategy->StopCurrentConnection(*this);
+        newConnectionStrategy->StopCurrentConnection(this);
     }
 
-    void SingleConnectionListener::StopCurrentConnection(SingleConnectionListener& listener)
+    void SingleConnectionListener::StopCurrentConnection(void* listener)
     {
         Stop([this]()
             {
