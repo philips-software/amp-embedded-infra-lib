97968e381cc18ce4efe564df60b93542b29820b9
ccrugoPhilips <cesar.cruz@philips.com>
2025-04-03 10:04:10 +0200

    Apply suggestions from code review
    Co-authored-by: Richard Peters <richard.peters@philips.com>

--- external/crypto/mbedtls/mbedtls_emil_config.h
+++ external/crypto/mbedtls/mbedtls_emil_config.h
@@ -4114,7 +4114,7 @@
  *
  * Uncomment to set the maximum plaintext size of the incoming I/O buffer.
  */
-#define MBEDTLS_SSL_IN_CONTENT_LEN              4096
+//#define MBEDTLS_SSL_IN_CONTENT_LEN              16384
 
 /** \def MBEDTLS_SSL_CID_IN_LEN_MAX
  *
@@ -4164,7 +4164,7 @@
  *
  * Uncomment to set the maximum plaintext size of the outgoing I/O buffer.
  */
-//#define MBEDTLS_SSL_OUT_CONTENT_LEN             16384
+#define MBEDTLS_SSL_OUT_CONTENT_LEN             4096
 
 /** \def MBEDTLS_SSL_DTLS_MAX_BUFFERING
  *
