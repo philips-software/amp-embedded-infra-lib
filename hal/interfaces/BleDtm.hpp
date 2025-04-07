fe9934b041a4eb0917c29325850e2b329a1408b9
Roy Creemers <roy.creemers@philips.com>
2025-04-07 06:57:37 +0000

    Process review comments

--- hal/interfaces/BleDtm.hpp
+++ hal/interfaces/BleDtm.hpp
@@ -9,12 +9,18 @@ namespace hal
     class BleDtm
     {
     public:
+        BleDtm() = default;
+        BleDtm(const BleDtm& other) = delete;
+        BleDtm& operator=(const BleDtm& other) = delete;
+        ~BleDtm() = default;
+
         virtual bool StartTone(uint8_t rfChannel, uint8_t offset) = 0;
         virtual bool StopTone() = 0;
         virtual bool SetTxPowerLevel(uint8_t txPower) = 0;
         virtual bool StartRxTest(uint8_t frequency, uint8_t phy) = 0;
         virtual bool StartTxTest(uint8_t frequency, uint8_t dataLength, uint8_t packetPayload, uint8_t phy) = 0;
-        virtual infra::Optional<uint16_t> StopTest();
+        virtual infra::Optional<uint16_t> StopTest(); // Returns the number of packets received during the test
+                                                      // If the test was not started or in case of error, returns none.
     };
 }
 
