79e99c6a816c6d18a274a92452c047768b80f545
oguzcan oguz <oguz.oguzcan@philips.com>
2025-05-01 15:18:59 +0000

    remove comments

--- services/ble/test/TestGattClientDecorator.cpp
+++ services/ble/test/TestGattClientDecorator.cpp
@@ -23,31 +23,6 @@ namespace
     };
 }
 
-// // Implementation of GattClientDiscovery
-// void StartServiceDiscovery() override;
-// void StartCharacteristicDiscovery(AttAttribute::Handle handle, AttAttribute::Handle endHandle) override;
-// void StartDescriptorDiscovery(AttAttribute::Handle handle, AttAttribute::Handle endHandle) override;
-
-// // Implementation of GattClientDiscoveryObserver
-// void ServiceDiscovered(const AttAttribute::Uuid& type, AttAttribute::Handle handle, AttAttribute::Handle endHandle) override;
-// void ServiceDiscoveryComplete() override;
-// void CharacteristicDiscovered(const AttAttribute::Uuid& type, AttAttribute::Handle handle, AttAttribute::Handle valueHandle, GattCharacteristic::PropertyFlags properties) override;
-// void CharacteristicDiscoveryComplete() override;
-// void DescriptorDiscovered(const AttAttribute::Uuid& type, AttAttribute::Handle handle) override;
-// void DescriptorDiscoveryComplete() override;
-
-// // Implementation of GattClientCharacteristicOperations
-// void Read(const GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void(const infra::ConstByteRange&)>& onRead, const infra::Function<void(uint8_t)>& onDone) override;
-// void Write(const GattClientCharacteristicOperationsObserver& characteristic, infra::ConstByteRange data, const infra::Function<void(uint8_t)>& onDone) override;
-// void WriteWithoutResponse(const GattClientCharacteristicOperationsObserver& characteristic, infra::ConstByteRange data) override;
-// void EnableNotification(const GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void(uint8_t)>& onDone) override;
-// void DisableNotification(const GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void(uint8_t)>& onDone) override;
-// void EnableIndication(const GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void(uint8_t)>& onDone) override;
-// void DisableIndication(const GattClientCharacteristicOperationsObserver& characteristic, const infra::Function<void(uint8_t)>& onDone) override;
-
-// // Implementation of GattClientCharacteristicOperationsObserver
-// AttAttribute::Handle CharacteristicValueHandle() const override;
-
 TEST_F(GattClientDecoratorTest, should_start_service_discovery)
 {
     EXPECT_CALL(discovery, StartServiceDiscovery());
