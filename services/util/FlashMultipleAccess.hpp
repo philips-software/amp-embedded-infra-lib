e69458d6c06144b553e1e996f4115c72af572c93
Richard Peters <richard.peters@philips.com>
2025-05-11 07:19:39 +0200

    feat: add services/util/FlashDelegate (#862)
    * feat: add services/util/FlashDelegate
    
    * Add tests

--- services/util/FlashMultipleAccess.hpp
+++ services/util/FlashMultipleAccess.hpp
@@ -4,6 +4,7 @@
 #include "hal/interfaces/Flash.hpp"
 #include "infra/event/ClaimableResource.hpp"
 #include "infra/util/AutoResetFunction.hpp"
+#include "services/util/FlashDelegate.hpp"
 
 #ifndef SERVICES_FLASH_MULTIPLE_ACCESS_FUNCTION_EXTRA_SIZE
 #define SERVICES_FLASH_MULTIPLE_ACCESS_FUNCTION_EXTRA_SIZE (INFRA_DEFAULT_FUNCTION_EXTRA_SIZE + (8 * sizeof(void*)))
@@ -12,22 +13,11 @@
 namespace services
 {
     class FlashMultipleAccessMaster
-        : public hal::Flash
+        : public FlashDelegate
         , public infra::ClaimableResource
     {
     public:
-        explicit FlashMultipleAccessMaster(hal::Flash& master);
-
-        uint32_t NumberOfSectors() const override;
-        uint32_t SizeOfSector(uint32_t sectorIndex) const override;
-        uint32_t SectorOfAddress(uint32_t address) const override;
-        uint32_t AddressOfSector(uint32_t sectorIndex) const override;
-        void WriteBuffer(infra::ConstByteRange buffer, uint32_t address, infra::Function<void()> onDone) override;
-        void ReadBuffer(infra::ByteRange buffer, uint32_t address, infra::Function<void()> onDone) override;
-        void EraseSectors(uint32_t beginIndex, uint32_t endIndex, infra::Function<void()> onDone) override;
-
-    private:
-        hal::Flash& master;
+        using FlashDelegate::FlashDelegate;
     };
 
     class FlashMultipleAccess
--- services/util/test/TestFlashMultipleAccess.cpp
+++ services/util/test/TestFlashMultipleAccess.cpp
@@ -14,62 +14,67 @@ public:
         , access2(multipleAccess)
     {}
 
-    testing::StrictMock<hal::FlashMock> flash;
+    testing::StrictMock<hal::CleanFlashMock> flash;
     services::FlashMultipleAccessMaster multipleAccess;
     services::FlashMultipleAccess access1;
     services::FlashMultipleAccess access2;
+
+    std::array<uint8_t, 1> buffer;
+    infra::Function<void()> onDone;
 };
 
 TEST_F(FlashMultipleAccessTest, Construction)
-{}
+{
+    EXPECT_CALL(flash, NumberOfSectors()).WillOnce(testing::Return(1));
+    EXPECT_EQ(1, access1.NumberOfSectors());
+
+    EXPECT_CALL(flash, SizeOfSector(2)).WillOnce(testing::Return(1));
+    EXPECT_EQ(1, access1.SizeOfSector(2));
+
+    EXPECT_CALL(flash, SectorOfAddress(2)).WillOnce(testing::Return(1));
+    EXPECT_EQ(1, access1.SectorOfAddress(2));
+
+    EXPECT_CALL(flash, AddressOfSector(2)).WillOnce(testing::Return(1));
+    EXPECT_EQ(1, access1.AddressOfSector(2));
+}
 
 TEST_F(FlashMultipleAccessTest, FirstReadIsExecuted)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(testing::_, 0, testing::_));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
     ExecuteAllActions();
 }
 
 TEST_F(FlashMultipleAccessTest, SecondReadIsNotExecuted)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(testing::_, 0, testing::_));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
-    access2.ReadBuffer(buffer, 0, infra::emptyFunction);
+    access2.ReadBuffer(buffer, 1, infra::emptyFunction);
     ExecuteAllActions();
 }
 
 TEST_F(FlashMultipleAccessTest, SecondReadIsExecutedWhenFirstAccessFinishes)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(infra::MakeRange(buffer), 0, testing::_)).WillOnce(testing::SaveArg<2>(&onDone));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
-    access2.ReadBuffer(buffer, 0, infra::emptyFunction);
+    access2.ReadBuffer(buffer, 1, infra::emptyFunction);
     ExecuteAllActions();
 
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
-    flash.done();
+    EXPECT_CALL(flash, ReadBuffer(infra::MakeRange(buffer), 1, testing::_));
+    onDone();
     ExecuteAllActions();
 }
 
 TEST_F(FlashMultipleAccessTest, FirstWriteIsExecuted)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, writeBufferMock(testing::_, testing::_));
+    EXPECT_CALL(flash, WriteBuffer(infra::MakeConst(infra::MakeRange(buffer)), 0, testing::_));
     access1.WriteBuffer(buffer, 0, infra::emptyFunction);
     ExecuteAllActions();
 }
 
 TEST_F(FlashMultipleAccessTest, SecondWriteIsNotExecuted)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(testing::_, 0, testing::_));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
     access2.WriteBuffer(buffer, 0, infra::emptyFunction);
     ExecuteAllActions();
@@ -77,30 +82,26 @@ TEST_F(FlashMultipleAccessTest, SecondWriteIsNotExecuted)
 
 TEST_F(FlashMultipleAccessTest, SecondWriteIsExecutedWhenFirstAccessFinishes)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(infra::MakeRange(buffer), 0, testing::_)).WillOnce(testing::SaveArg<2>(&onDone));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
-    access2.WriteBuffer(buffer, 0, infra::emptyFunction);
+    access2.WriteBuffer(buffer, 1, infra::emptyFunction);
     ExecuteAllActions();
 
-    EXPECT_CALL(flash, writeBufferMock(testing::_, testing::_));
-    flash.done();
+    EXPECT_CALL(flash, WriteBuffer(infra::MakeConst(infra::MakeRange(buffer)), 1, testing::_));
+    onDone();
     ExecuteAllActions();
 }
 
 TEST_F(FlashMultipleAccessTest, FirstEraseIsExecuted)
 {
-    EXPECT_CALL(flash, eraseSectorsMock(testing::_, testing::_));
+    EXPECT_CALL(flash, EraseSectors(0, 1, testing::_));
     access1.EraseSectors(0, 1, infra::emptyFunction);
     ExecuteAllActions();
 }
 
 TEST_F(FlashMultipleAccessTest, SecondEraseIsNotExecuted)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(infra::MakeRange(buffer), 0, testing::_));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
     access2.EraseSectors(0, 1, infra::emptyFunction);
     ExecuteAllActions();
@@ -108,14 +109,12 @@ TEST_F(FlashMultipleAccessTest, SecondEraseIsNotExecuted)
 
 TEST_F(FlashMultipleAccessTest, SecondEraseIsExecutedWhenFirstAccessFinishes)
 {
-    std::array<uint8_t, 1> buffer;
-
-    EXPECT_CALL(flash, readBufferMock(testing::_)).WillOnce(testing::Return(std::vector<uint8_t>(1, 0)));
+    EXPECT_CALL(flash, ReadBuffer(infra::MakeRange(buffer), 0, testing::_)).WillOnce(testing::SaveArg<2>(&onDone));
     access1.ReadBuffer(buffer, 0, infra::emptyFunction);
     access2.EraseSectors(0, 1, infra::emptyFunction);
     ExecuteAllActions();
 
-    EXPECT_CALL(flash, eraseSectorsMock(testing::_, testing::_));
-    flash.done();
+    EXPECT_CALL(flash, EraseSectors(0, 1, testing::_));
+    onDone();
     ExecuteAllActions();
 }
