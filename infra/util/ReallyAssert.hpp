ea9ead08fa432e776fd2676379060486be7337f4
Gabriel Santos <gabriel.santos_1@philips.com>
2025-09-25 11:27:09 +0000

    throw exceptions when is compiled for host platform

--- infra/util/ReallyAssert.hpp
+++ infra/util/ReallyAssert.hpp
@@ -6,7 +6,30 @@
 #include <cassert>
 #include <cstdlib>
 
+#ifdef EMIL_HOST_BUILD
+#include <stdexcept>
+#include <string>
+
+namespace infra
+{
+    [[noreturn]] inline void HandleAssertionFailure(const char* condition, const char* file, int line)
+    {
+        throw std::runtime_error(std::string("Assertion failed: ") + condition +
+                                 " at " + file + ":" + std::to_string(line));
+    }
+}
+#endif
+
 #ifdef NDEBUG
+#ifdef EMIL_HOST_BUILD
+#define really_assert(condition)                                       \
+    if (!(condition))                                                  \
+    {                                                                  \
+        infra::HandleAssertionFailure(#condition, __FILE__, __LINE__); \
+    }                                                                  \
+    else                                                               \
+        for (; false;)
+#else
 #define really_assert(condition) \
     if (!(condition))            \
     {                            \
@@ -14,8 +37,19 @@
     }                            \
     else                         \
         for (; false;)
+#endif
+#else
+#ifdef EMIL_HOST_BUILD
+#define really_assert(condition)                                       \
+    if (!(condition))                                                  \
+    {                                                                  \
+        infra::HandleAssertionFailure(#condition, __FILE__, __LINE__); \
+    }                                                                  \
+    else                                                               \
+        for (; false;)
 #else
 #define really_assert(condition) assert(condition)
+#endif
 #endif
 
 #endif
