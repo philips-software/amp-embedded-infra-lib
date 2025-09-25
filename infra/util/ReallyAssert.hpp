cdcbdfacb16bfaead5e0d963041115c21a6a100d
Gabriel Santos <gabriel.santos_1@philips.com>
2025-09-25 12:25:57 +0000

    add assertion failure handler when really assert is invoked

--- infra/util/ReallyAssert.hpp
+++ infra/util/ReallyAssert.hpp
@@ -6,7 +6,29 @@
 #include <cassert>
 #include <cstdlib>
 
+#ifdef EMIL_HOST_BUILD
+#include <functional>
+
+namespace infra
+{
+    using AssertionFailureHandler = std::function<void(const char* condition, const char* file, int line)>;
+
+    void RegisterAssertionFailureHandler(AssertionFailureHandler handler);
+
+    [[noreturn]] void HandleAssertionFailure(const char* condition, const char* file, int line);
+}
+#endif
+
 #ifdef NDEBUG
+#ifdef EMIL_HOST_BUILD
+#define really_assert(condition)                                       \
+    if (!(condition))                                                  \
+    {                                                                  \
+        infra::HandleAssertionFailure(#condition, __FILE__, __LINE__); \
+    }                                                                  \
+    else                                                               \
+        for (; false;)
+#else
 #define really_assert(condition) \
     if (!(condition))            \
     {                            \
@@ -14,8 +36,19 @@
     }                            \
     else                         \
         for (; false;)
+#endif
+#else
+#ifdef EMIL_HOST_BUILD
+#define really_assert(condition)                                       \
+    if (!(condition))                                                  \
+    {                                                                  \
+        infra::HandleAssertionFailure(#condition, __FILE__, __LINE__); \
+    }                                                                  \
+    else                                                               \
+        for (; false;)
 #else
 #define really_assert(condition) assert(condition)
+#endif
 #endif
 
 #endif
