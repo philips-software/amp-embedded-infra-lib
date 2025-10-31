d5141e5cea9a7089a0a23b50a7212b01e4be1c9f
Daniel Schenk <daniel.schenk@philips.com>
2025-10-31 10:04:29 +0000

    typed test (hopefully resolves coverage issue)

--- services/util/test/TestSleepAfterOperationFlashDecorator.cpp
+++ services/util/test/TestSleepAfterOperationFlashDecorator.cpp
@@ -7,6 +7,7 @@
 
 namespace services
 {
+    template<typename T>
     class SleepAfterOperationFlashDecoratorTest : public testing::Test
     {
     public:
@@ -20,80 +21,83 @@ namespace services
             ON_CALL(sleepable, Wake(::testing::_)).WillByDefault(::testing::InvokeArgument<0>());
         }
 
-        ::testing::StrictMock<hal::CleanFlashMock> flash;
+        ::testing::StrictMock<hal::CleanFlashMockBase<T>> flash;
         ::testing::StrictMock<hal::SleepableMock> sleepable;
-        SleepAfterOperationFlashDecorator decorator{ flash, sleepable };
+        SleepAfterOperationFlashDecoratorBase<T> decorator{ flash, sleepable };
 
         std::array<uint8_t, 32> dummyArray;
     };
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, number_of_sectors_forwards_result)
+    typedef ::testing::Types<uint32_t, uint64_t> FlashTypes;
+    TYPED_TEST_SUITE(SleepAfterOperationFlashDecoratorTest, FlashTypes);
+
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, number_of_sectors_forwards_result)
     {
         uint32_t nr = 42;
-        EXPECT_CALL(flash, NumberOfSectors()).WillOnce(::testing::Return(nr));
-        EXPECT_EQ(decorator.NumberOfSectors(), nr);
+        EXPECT_CALL(this->flash, NumberOfSectors()).WillOnce(::testing::Return(nr));
+        EXPECT_EQ(this->decorator.NumberOfSectors(), nr);
     }
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, size_of_sector_forwards_args_and_result)
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, size_of_sector_forwards_args_and_result)
     {
         uint32_t size = 4096;
-        EXPECT_CALL(flash, SizeOfSector(5)).WillOnce(::testing::Return(size));
-        EXPECT_EQ(decorator.SizeOfSector(5), size);
+        EXPECT_CALL(this->flash, SizeOfSector(5)).WillOnce(::testing::Return(size));
+        EXPECT_EQ(this->decorator.SizeOfSector(5), size);
     }
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, sector_of_address_forwards_args_and_result)
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, sector_of_address_forwards_args_and_result)
     {
         uint32_t sector = 7;
-        EXPECT_CALL(flash, SectorOfAddress(0x1000)).WillOnce(::testing::Return(sector));
-        EXPECT_EQ(decorator.SectorOfAddress(0x1000), sector);
+        EXPECT_CALL(this->flash, SectorOfAddress(0x1000)).WillOnce(::testing::Return(sector));
+        EXPECT_EQ(this->decorator.SectorOfAddress(0x1000), sector);
     }
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, address_of_sector_forwards_args_and_result)
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, address_of_sector_forwards_args_and_result)
     {
         uint32_t address = 0x2000;
-        EXPECT_CALL(flash, AddressOfSector(3)).WillOnce(::testing::Return(address));
-        EXPECT_EQ(decorator.AddressOfSector(3), address);
+        EXPECT_CALL(this->flash, AddressOfSector(3)).WillOnce(::testing::Return(address));
+        EXPECT_EQ(this->decorator.AddressOfSector(3), address);
     }
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, write_buffer_does_wake_write_sleep)
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, write_buffer_does_wake_write_sleep)
     {
         uint32_t address = 42;
-        auto buffer = infra::MakeConstByteRange(dummyArray);
+        auto buffer = infra::MakeConstByteRange(this->dummyArray);
 
         ::testing::InSequence inSequenceEnabler;
-        EXPECT_CALL(sleepable, Wake(::testing::_));
-        EXPECT_CALL(flash, WriteBuffer(buffer, address, ::testing::_));
-        EXPECT_CALL(sleepable, Sleep(::testing::_));
+        EXPECT_CALL(this->sleepable, Wake(::testing::_));
+        EXPECT_CALL(this->flash, WriteBuffer(buffer, address, ::testing::_));
+        EXPECT_CALL(this->sleepable, Sleep(::testing::_));
 
         infra::VerifyingFunction<void()> onDone;
-        decorator.WriteBuffer(buffer, address, onDone);
+        this->decorator.WriteBuffer(buffer, address, onDone);
     }
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, read_buffer_does_wake_read_sleep)
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, read_buffer_does_wake_read_sleep)
     {
         uint32_t address = 42;
-        auto buffer = infra::MakeByteRange(dummyArray);
+        auto buffer = infra::MakeByteRange(this->dummyArray);
 
         ::testing::InSequence inSequenceEnabler;
-        EXPECT_CALL(sleepable, Wake(::testing::_));
-        EXPECT_CALL(flash, ReadBuffer(buffer, address, ::testing::_));
-        EXPECT_CALL(sleepable, Sleep(::testing::_));
+        EXPECT_CALL(this->sleepable, Wake(::testing::_));
+        EXPECT_CALL(this->flash, ReadBuffer(buffer, address, ::testing::_));
+        EXPECT_CALL(this->sleepable, Sleep(::testing::_));
 
         infra::VerifyingFunction<void()> onDone;
-        decorator.ReadBuffer(buffer, address, onDone);
+        this->decorator.ReadBuffer(buffer, address, onDone);
     }
 
-    TEST_F(SleepAfterOperationFlashDecoratorTest, erase_sectors_does_wake_erase_sleep)
+    TYPED_TEST(SleepAfterOperationFlashDecoratorTest, erase_sectors_does_wake_erase_sleep)
     {
         uint32_t beginIndex = 2;
         uint32_t endIndex = 5;
 
         ::testing::InSequence inSequenceEnabler;
-        EXPECT_CALL(sleepable, Wake(::testing::_));
-        EXPECT_CALL(flash, EraseSectors(beginIndex, endIndex, ::testing::_));
-        EXPECT_CALL(sleepable, Sleep(::testing::_));
+        EXPECT_CALL(this->sleepable, Wake(::testing::_));
+        EXPECT_CALL(this->flash, EraseSectors(beginIndex, endIndex, ::testing::_));
+        EXPECT_CALL(this->sleepable, Sleep(::testing::_));
 
         infra::VerifyingFunction<void()> onDone;
-        decorator.EraseSectors(beginIndex, endIndex, onDone);
+        this->decorator.EraseSectors(beginIndex, endIndex, onDone);
     }
 }
