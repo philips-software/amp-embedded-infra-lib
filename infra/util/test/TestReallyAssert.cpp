5545bef9622fb4a2d17d05fd79dad33267191023
Hein Wessels <hein.wessels@philips.com>
2025-11-17 14:48:45 +0000

    Remove some comments

--- infra/util/test/TestReallyAssert.cpp
+++ infra/util/test/TestReallyAssert.cpp
@@ -20,7 +20,7 @@ namespace
 
 TEST_F(ReallyAssertTest, assert_passed_no_abort)
 {
-    really_assert(true); // Should not abort
+    really_assert(true);
 }
 
 TEST_F(ReallyAssertTest, assert_failed_abort)
@@ -28,13 +28,13 @@ TEST_F(ReallyAssertTest, assert_failed_abort)
     EXPECT_DEATH(really_assert(false), "");
 }
 
-TEST_F(ReallyAssertTest, assert_failed_without_handler)
+TEST_F(ReallyAssertTest, assert_failed_without_handler_does_nothing)
 {
     // Manually calling handler because a debug build will call the standard assert instead of really_assert
-    infra::HandleAssertionFailure("condition", "file", 42); // Does nothing
+    infra::HandleAssertionFailure("condition", "file", 42);
 }
 
-TEST_F(ReallyAssertTest, assert_failed_with_handler)
+TEST_F(ReallyAssertTest, assert_failed_with_handler_calls_handler)
 {
     infra::RegisterAssertionFailureHandler([&](auto condition, auto file, auto line)
         {
