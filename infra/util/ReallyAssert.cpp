f703210fcb543a39166e5052922aa695e416b0b1
Hein Wessels <hein.wessels@philips.com>
2026-01-06 09:19:46 +0000

    Simplify really_assert for easier testing

--- infra/util/ReallyAssert.cpp
+++ infra/util/ReallyAssert.cpp
@@ -13,10 +13,10 @@ namespace infra
 
     void HandleAssertionFailure(const char* condition, const char* file, int line)
     {
-        if constexpr (INFRA_UTIL_LOG_AND_ABORT_ENABLED)
-            infra::ExecuteLogAndAbortHook("Assertion failed [%s] at %s:%d\n", condition, file, line);
-        else if (customHandler)
+        if (customHandler)
             customHandler(condition, file, line);
+        else if constexpr (INFRA_UTIL_LOG_AND_ABORT_ENABLED)
+            infra::ExecuteLogAndAbortHook("Assertion failed [%s] at %s:%d\n", condition, file, line);
     }
 }
 #endif
