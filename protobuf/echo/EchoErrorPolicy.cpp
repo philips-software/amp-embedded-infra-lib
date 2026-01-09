93d46b617b3a02a4b8889f6ff59eb1ffe000f47d
Xavier Bernhardt <xavier.bernhardt@philips.com>
2026-01-09 13:59:08 +0000

    logs for servicenotfound

--- protobuf/echo/EchoErrorPolicy.cpp
+++ protobuf/echo/EchoErrorPolicy.cpp
@@ -1,4 +1,6 @@
 #include "protobuf/echo/EchoErrorPolicy.hpp"
+#include "infra/stream/StdStringOutputStream.hpp"
+#include "infra/util/ReallyAssert.hpp"
 #include <cstdlib>
 
 namespace services
@@ -19,11 +21,17 @@ namespace services
 
     void EchoErrorPolicyAbort::ServiceNotFound(uint32_t serviceId) const
     {
+        infra::StdStringOutputStream::WithStorage stream;
+        stream << "EchoErrorPolicyAbort::ServiceNotFound - serviceId: " << serviceId;
+        really_assert(false);
         std::abort();
     }
 
     void EchoErrorPolicyAbort::MethodNotFound(uint32_t serviceId, uint32_t methodId) const
     {
+        infra::StdStringOutputStream::WithStorage stream;
+        stream << "EchoErrorPolicyAbort::MethodNotFound - serviceId: " << serviceId << ", methodId: " << methodId;
+        really_assert(false);
         std::abort();
     }
 }
