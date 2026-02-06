cba3559961c100a1129d539c19d1c47c7bad0f94
Richard Peters <richard.peters@philips.com>
2026-02-06 14:20:30 +0100

    refactor: remove EchoWithPolicy (#1082)
    * refactor: remove EchoWithPolicy
    
    * Add TestEchoOnStreams
    
    * Apply suggestions from code review
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    * Update infra/stream/test/TestStreamWriterFile.cpp
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: oguzcan oguz <oguzcanoguz82@gmail.com>

--- services/util/EchoPolicyDiffieHellman.hpp
+++ services/util/EchoPolicyDiffieHellman.hpp
@@ -49,7 +49,7 @@ namespace services
         struct WithCryptoMbedTls;
 #endif
 
-        EchoPolicyDiffieHellman(const Crypto& crypto, EchoWithPolicy& echo, EchoInitialization& echoInitialization, SesameSecured& secured, infra::ConstByteRange dsaCertificate, infra::ConstByteRange rootCaCertificate, hal::SynchronousRandomDataGenerator& randomDataGenerator);
+        EchoPolicyDiffieHellman(const Crypto& crypto, Echo& echo, EchoInitialization& echoInitialization, SesameSecured& secured, infra::ConstByteRange dsaCertificate, infra::ConstByteRange rootCaCertificate, hal::SynchronousRandomDataGenerator& randomDataGenerator);
 
     private:
         // Implementation of SesameObserver
@@ -94,7 +94,7 @@ namespace services
     struct EchoPolicyDiffieHellman::WithCryptoMbedTls
         : public EchoPolicyDiffieHellman
     {
-        WithCryptoMbedTls(EchoWithPolicy& echo, EchoInitialization& echoInitialization, SesameSecured& secured, const EchoPolicyDiffieHellman::KeyMaterial& keyMaterial, hal::SynchronousRandomDataGenerator& randomDataGenerator);
+        WithCryptoMbedTls(Echo& echo, EchoInitialization& echoInitialization, SesameSecured& secured, const EchoPolicyDiffieHellman::KeyMaterial& keyMaterial, hal::SynchronousRandomDataGenerator& randomDataGenerator);
 
         infra::Creator<EcSecP256r1DiffieHellman, EcSecP256r1DiffieHellmanMbedTls, void(hal::SynchronousRandomDataGenerator& randomDataGenerator)> keyExchange;
         EcSecP256r1DsaSignerMbedTls signer;
