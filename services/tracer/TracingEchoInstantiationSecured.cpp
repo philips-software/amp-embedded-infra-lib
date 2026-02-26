11ce0fbdcf8c0129bad03dfef651fd4f8190062a
heinwessels <hein.wessels@philips.com>
2026-02-26 16:35:31 +0100

    feat: echo error policy that only warns (#1117)
    * Add way to have echo error policy that only warns
    
    * Add unit tests
    
    * Update protobuf/echo/EchoErrorPolicy.hpp
    
    Co-authored-by: André Jordan <3933856+andjordan@users.noreply.github.com>
    
    * Put sources in right orderg
    
    ---------
    
    Co-authored-by: André Jordan <3933856+andjordan@users.noreply.github.com>

--- services/tracer/TracingEchoInstantiationSecured.cpp
+++ services/tracer/TracingEchoInstantiationSecured.cpp
@@ -1,12 +1,13 @@
 #include "services/tracer/TracingEchoInstantiationSecured.hpp"
+#include "protobuf/echo/EchoErrorPolicy.hpp"
 
 namespace main_
 {
     TracingEchoOnSesameSecured::TracingEchoOnSesameSecured(infra::BoundedVector<uint8_t>& cobsSendStorage, infra::BoundedDeque<uint8_t>& cobsReceivedMessage, infra::BoundedVector<uint8_t>& securedSendBuffer, infra::BoundedVector<uint8_t>& securedReceiveBuffer,
-        hal::BufferedSerialCommunication& serialCommunication, services::MethodSerializerFactory& serializerFactory, const services::SesameSecured::KeyMaterial& keyMaterial, services::Tracer& tracer)
+        hal::BufferedSerialCommunication& serialCommunication, services::MethodSerializerFactory& serializerFactory, const services::SesameSecured::KeyMaterial& keyMaterial, const services::EchoErrorPolicy& echoErrorPolicy, services::Tracer& tracer)
         : cobs(cobsSendStorage, cobsReceivedMessage, serialCommunication)
         , secured(securedSendBuffer, securedReceiveBuffer, windowed, keyMaterial)
-        , echo(serializerFactory, services::echoErrorPolicyAbort, tracer, secured)
+        , echo(serializerFactory, echoErrorPolicy, tracer, secured)
     {}
 
     void TracingEchoOnSesameSecured::Reset()
@@ -26,13 +27,13 @@ namespace main_
 
     TracingEchoOnSesameSecuredSymmetricKey::TracingEchoOnSesameSecuredSymmetricKey(infra::BoundedVector<uint8_t>& cobsSendStorage, infra::BoundedDeque<uint8_t>& cobsReceivedMessage, infra::BoundedVector<uint8_t>& securedSendBuffer, infra::BoundedVector<uint8_t>& securedReceiveBuffer,
         hal::BufferedSerialCommunication& serialCommunication, services::MethodSerializerFactory& serializerFactory, const services::SesameSecured::KeyMaterial& keyMaterial, hal::SynchronousRandomDataGenerator& randomDataGenerator, services::Tracer& tracer)
-        : TracingEchoOnSesameSecured(cobsSendStorage, cobsReceivedMessage, securedSendBuffer, securedReceiveBuffer, serialCommunication, serializerFactory, keyMaterial, tracer)
+        : TracingEchoOnSesameSecured(cobsSendStorage, cobsReceivedMessage, securedSendBuffer, securedReceiveBuffer, serialCommunication, serializerFactory, keyMaterial, services::echoErrorPolicyAbort, tracer)
         , policy(echo, echo, secured, randomDataGenerator)
     {}
 
     TracingEchoOnSesameSecuredDiffieHellman::TracingEchoOnSesameSecuredDiffieHellman(infra::BoundedVector<uint8_t>& cobsSendStorage, infra::BoundedDeque<uint8_t>& cobsReceivedMessage, infra::BoundedVector<uint8_t>& securedSendBuffer, infra::BoundedVector<uint8_t>& securedReceiveBuffer,
-        hal::BufferedSerialCommunication& serialCommunication, services::MethodSerializerFactory& serializerFactory, const services::EchoPolicyDiffieHellman::Crypto& crypto, infra::ConstByteRange dsaCertificate, infra::ConstByteRange rootCaCertificate, hal::SynchronousRandomDataGenerator& randomDataGenerator, services::Tracer& tracer)
-        : TracingEchoOnSesameSecured(cobsSendStorage, cobsReceivedMessage, securedSendBuffer, securedReceiveBuffer, serialCommunication, serializerFactory, services::SesameSecured::KeyMaterial{}, tracer)
+        hal::BufferedSerialCommunication& serialCommunication, services::MethodSerializerFactory& serializerFactory, const services::EchoPolicyDiffieHellman::Crypto& crypto, infra::ConstByteRange dsaCertificate, infra::ConstByteRange rootCaCertificate, hal::SynchronousRandomDataGenerator& randomDataGenerator, const services::EchoErrorPolicy& echoErrorPolicy, services::Tracer& tracer)
+        : TracingEchoOnSesameSecured(cobsSendStorage, cobsReceivedMessage, securedSendBuffer, securedReceiveBuffer, serialCommunication, serializerFactory, services::SesameSecured::KeyMaterial{}, echoErrorPolicy, tracer)
         , policy(crypto, echo, echo, secured, dsaCertificate, rootCaCertificate, randomDataGenerator)
     {}
 }
