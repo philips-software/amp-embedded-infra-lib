1bf50acef4d58550316571b0984e2f3886264f72
ccrugoPhilips <cesar.cruz@philips.com>
2026-05-19 11:15:26 +0000

    fix: use POSIX timer with realtime signal for mutation testing timeout guard

--- services/util/test/MutationTestingTimeoutGuard.cpp
+++ services/util/test/MutationTestingTimeoutGuard.cpp
@@ -1,6 +1,7 @@
 #ifdef EMIL_MUTATION_TESTING
 
 #include <csignal>
+#include <ctime>
 #include <unistd.h>
 
 static void MutationTimeoutHandler(int)
@@ -8,14 +9,24 @@ static void MutationTimeoutHandler(int)
     _exit(1);
 }
 
-__attribute__((constructor(65535))) static void SetupMutationTimeoutGuard()
+__attribute__((constructor(101))) static void SetupMutationTimeoutGuard()
 {
     struct sigaction sa = {};
     sa.sa_handler = MutationTimeoutHandler;
     sa.sa_flags = 0;
     sigemptyset(&sa.sa_mask);
-    sigaction(SIGALRM, &sa, nullptr);
-    alarm(8);
+    sigaction(SIGRTMIN + 1, &sa, nullptr);
+
+    struct sigevent sev = {};
+    sev.sigev_notify = SIGEV_SIGNAL;
+    sev.sigev_signo = SIGRTMIN + 1;
+
+    timer_t timer;
+    timer_create(CLOCK_MONOTONIC, &sev, &timer);
+
+    struct itimerspec its = {};
+    its.it_value.tv_sec = 8;
+    timer_settime(timer, 0, &its, nullptr);
 }
 
 #endif
