10d50021c76add2e496c8eaf8fd7e0ebc47f6336
Richard Peters <richard.peters@philips.com>
2026-05-20 08:52:41 +0200

    feat: add TracerWithPrefix

---
+++ services/tracer/test/TestTracerWithPrefix.cpp
@@ -0,0 +1,41 @@
+#include "infra/stream/StringOutputStream.hpp"
+#include "services/tracer/TracerWithPrefix.hpp"
+#include "gmock/gmock.h"
+
+class TracerWithPrefixTest
+    : public testing::Test
+{};
+
+TEST(TracerWithPrefixTest, prefix_is_inserted_before_message)
+{
+    infra::StringOutputStream::WithStorage<64> stream;
+    services::TracerToStream baseTracer(stream);
+    services::TracerWithPrefix tracer("prefix: ", baseTracer);
+
+    tracer.Trace() << "message";
+
+    EXPECT_EQ("\r\nprefix: message", stream.Storage());
+}
+
+TEST(TracerWithPrefixTest, empty_prefix_leaves_output_unchanged)
+{
+    infra::StringOutputStream::WithStorage<64> stream;
+    services::TracerToStream baseTracer(stream);
+    services::TracerWithPrefix tracer("", baseTracer);
+
+    tracer.Trace() << "message";
+
+    EXPECT_EQ("\r\nmessage", stream.Storage());
+}
+
+TEST(TracerWithPrefixTest, prefix_is_repeated_on_each_trace)
+{
+    infra::StringOutputStream::WithStorage<64> stream;
+    services::TracerToStream baseTracer(stream);
+    services::TracerWithPrefix tracer("[mod] ", baseTracer);
+
+    tracer.Trace() << "first";
+    tracer.Trace() << "second";
+
+    EXPECT_EQ("\r\n[mod] first\r\n[mod] second", stream.Storage());
+}
