7221598c9c1add845203679314f3eb3c484c74fa
Richard Peters <richard.peters@philips.com>
2026-05-22 08:24:25 +0200

    Resolve review comments

--- services/util/EchoPolicyDiffieHellman.cpp
+++ services/util/EchoPolicyDiffieHellman.cpp
@@ -39,6 +39,7 @@ namespace services
     void EchoPolicyDiffieHellman::Reset()
     {
         busy = false;
+        initializingKeys = true;
     }
 
     void EchoPolicyDiffieHellman::Initialized()
--- services/util/EchoPolicyDiffieHellman.hpp
+++ services/util/EchoPolicyDiffieHellman.hpp
@@ -52,7 +52,7 @@ namespace services
         EchoPolicyDiffieHellman(const Crypto& crypto, Echo& echo, EchoInitialization& echoInitialization, SesameSecured& secured, infra::ConstByteRange dsaCertificate, infra::ConstByteRange rootCaCertificate, hal::SynchronousRandomDataGenerator& randomDataGenerator);
 
     private:
-        // Implementation of SesameObserver
+        // Implementation of EchoInitializationObserver
         void Reset() override;
         void Initialized() override;
 
