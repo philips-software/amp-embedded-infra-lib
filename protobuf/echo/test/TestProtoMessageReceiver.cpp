cad30825f7172b27b8841f91793e338b9c27d206
Hein Wessels <hein.wessels@philips.com>
2026-06-10 10:12:16 +0000

    Fix my use case. Test not yet passing, but is compiling

--- protobuf/echo/test/TestProtoMessageReceiver.cpp
+++ protobuf/echo/test/TestProtoMessageReceiver.cpp
@@ -158,6 +158,19 @@ TEST(ProtoMessageReceiverTest, parse_unbounded_repeated_uint32)
     EXPECT_EQ((std::vector<uint32_t>{ { 5, 6 } }), receiver.message.value);
 }
 
+TEST(ProtoMessageReceiverTest, parse_unbounded_repeated_nested_message)
+{
+    services::ProtoMessageReceiver<test_messages::TestNestedRepeatedMessage> receiver;
+
+    infra::StdVectorInputStreamReader::WithStorage data(std::in_place, std::initializer_list<uint8_t>{ 1 << 3, 5, 1 << 3, 6 });
+    receiver.Feed(data);
+
+    test_messages::TestNestedRepeatedMessage expected;
+    expected.message.push_back({ 5 });
+    expected.message.push_back({ 6 });
+    EXPECT_EQ(expected, receiver.message);
+}
+
 TEST(ProtoMessageReceiverTest, parse_incomplete_int)
 {
     services::ProtoMessageReceiver<test_messages::TestInt32> receiver;
