52bf2497c0f5ad762a160132e8746922d4a1c115
heinwessels <hein.wessels@philips.com>
2026-06-29 14:37:45 +0200

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- infra/timer/TimerService.cpp
+++ infra/timer/TimerService.cpp
@@ -1,6 +1,7 @@
 #include "infra/timer/TimerService.hpp"
 #include "infra/util/ReallyAssert.hpp"
 #include "infra/util/LogAndAbort.hpp"
+#include "infra/util/ReallyAssert.hpp"
 #include <cinttypes>
 
 namespace infra
@@ -12,8 +13,8 @@ namespace infra
     {
         for (const TimerService& timerService : timerServices)
             really_assert_with_msg(timerService.Id() != id, "Duplicate timer service id: %" PRIu32, id);
-        
-        timerServices.push_front(*this); 
+
+        timerServices.push_front(*this);
     }
 
     TimerService::~TimerService()
