f3a425861585aa68ce8a4a010ea3852d953383f4
André Jordan <3933856+andjordan@users.noreply.github.com>
2026-08-26 08:15:54 +0000

    test: helper for echo tests with deferred requests

---
+++ protobuf/echo/test_doubles/DeferredSendEchoPolicy.cpp
@@ -0,0 +1,33 @@
+#include "protobuf/echo/test_doubles/DeferredSendEchoPolicy.hpp"
+
+namespace services
+{
+    void DeferredSendEchoPolicy::RequestSend(ServiceProxy& proxy, const infra::Function<void(ServiceProxy& proxy)>& onRequest)
+    {
+        if (deferring)
+            deferredRequests.emplace_back(&proxy, onRequest);
+        else
+            onRequest(proxy);
+    }
+
+    void DeferredSendEchoPolicy::StartDeferring()
+    {
+        deferring = true;
+    }
+
+    void DeferredSendEchoPolicy::GrantDeferredRequests()
+    {
+        deferring = false;
+
+        auto requests = std::move(deferredRequests);
+        deferredRequests.clear();
+
+        for (auto& [proxy, onRequest] : requests)
+            onRequest(*proxy);
+    }
+
+    const std::vector<DeferredSendEchoPolicy::DeferredRequest>& DeferredSendEchoPolicy::DeferredRequests() const
+    {
+        return deferredRequests;
+    }
+}
